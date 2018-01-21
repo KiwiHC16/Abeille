6bf8623b228279a25406e8807d6bc361ffb12685
edgd <edgd1er@hotmail.com>
2018-01-21 11:14:41 +0100

    - detect docker, use /etc/init.d unstead of service. - start mosqitto service

--- resources/install.sh
+++ resources/install.sh
@@ -40,13 +40,13 @@ if [[ -d "/etc/php5/" ]]; then
   fi
   if [[ -d "/etc/php5/fpm/" && ! `cat /etc/php5/fpm/php.ini | grep "mosquitto"` ]]; then
   	echo "extension=mosquitto.so" | tee -a /etc/php5/fpm/php.ini
-    service php5-fpm restart
+    SERVICE="php5-fpm"
   fi
   if [[ -d "/etc/php5/apache2/" && ! `cat /etc/php5/apache2/php.ini | grep "mosquitto"` ]]; then
   	echo "extension=mosquitto.so" | tee -a /etc/php5/apache2/php.ini
     rm /tmp/Abeille_dep
     echo "Fin installation des dépendances"
-    service apache2 restart
+    SERVICE="apache2"
   fi
 else
   apt-get -y install php7.0-dev
@@ -57,14 +57,28 @@ else
   fi
   if [[ -d "/etc/php/7.0/fpm/" && ! `cat /etc/php/7.0/fpm/php.ini | grep "mosquitto"` ]]; then
     echo "extension=mosquitto.so" | tee -a /etc/php/7.0/fpm/php.ini
-    service php5-fpm restart
+    SERVICE="php5-fpm"
   fi
   if [[ -d "/etc/php/7.0/apache2/" && ! `cat /etc/php/7.0/apache2/php.ini | grep "mosquitto"` ]]; then
     echo "extension=mosquitto.so" | tee -a /etc/php/7.0/apache2/php.ini
     rm /tmp/Abeille_dep
-    echo "Fin installation des dépendances"
-    service apache2 restart
+    SERVICE="apache2"
   fi
+fi
+
+# Docker detection, may be useful to add RPI detection here.
+if [[ $(grep -c docker /proc/1/cgroup) -gt 0 ]]; then
+   echo "I'm running on docker".
+   /etc/init.d/mosquitto start
+   if [[ "apache2" == ${SERVICE} ]]; then
+        apache2ctl restart
+        else
+        /etc/init.d/${SERVICE} restart
+        fi
+    else
+    system mosquitto start
+    service ${SERVICE} restart
+
 fi
 
 rm /tmp/Abeille_dep

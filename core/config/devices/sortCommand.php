19bc906f9c5b06eb561cb5b9727bbc1d48a1dd36
KiwiHC16 <moulinb@free.fr>
2018-05-24 15:57:17 +0200

    Re-ordered all remaining files

--- core/config/devices/sortCommand.php
+++ core/config/devices/sortCommand.php
@@ -1,6 +1,10 @@
 <?php
     if ( strlen($argv[1]) == 0 ) {
         echo "usage: php sortCommand.php file.json\n";
+        echo "Commande are ordered by order of appearance in the file\n";
+        echo "JSON file should have one parametre/value per row.\n";
+        echo 'Pour faire tous les fichier: find . -name "[!.]*.json" -print -exec bash -c "eval php sortCommand.php {} > {}.new" \;'."\n";
+        echo 'Pour remplacer les anciens par les nouveaux:find . -name "[!.]*.json" -print -exec mv {}.new {} \;'."\n";
         exit;
     }
 
@@ -25,5 +29,5 @@
         echo "Can t open the file.\n";
     }
     
-    echo "\n";
+
 ?>

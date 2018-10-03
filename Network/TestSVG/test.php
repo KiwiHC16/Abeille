1b18d0e38a9c38ad49354c657a8723c9835aa32a
KiwiHC16 <moulinb@free.fr>
2018-10-03 16:46:30 +0200

    Error message resolution + Abeille name

---
+++ Network/TestSVG/test.php
@@ -0,0 +1,22 @@
+<?php
+    
+    require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+    
+    $json = "[{ ";
+    
+    $eqLogics = eqLogic::byType('Abeille');
+    foreach ($eqLogics as $eqLogic) {
+        $name = $eqLogic->getName();
+        $shortAddress = str_replace("Abeille/", "", $eqLogic->getLogicalId());
+        $shortAddress = ($name == 'Ruche') ? "0000" : $shortAddress;
+        
+        $json = $json . '"' . $shortAddress.'": "'.$name.'", ';
+    }
+    
+    $json = substr($json,0,-2) . ' }] ';
+    
+    echo $json;
+    
+    
+    ?>
+

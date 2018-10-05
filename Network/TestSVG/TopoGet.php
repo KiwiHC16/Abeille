7ea814fb64a097616323f4c694fab6582c7c952e
KiwiHC16 <moulinb@free.fr>
2018-10-05 16:55:45 +0200

    #387 Exclude Timers

--- Network/TestSVG/TopoGet.php
+++ Network/TestSVG/TopoGet.php
@@ -6,13 +6,18 @@
     
     $eqLogics = eqLogic::byType('Abeille');
     foreach ($eqLogics as $eqLogic) {
-        $name = $eqLogic->getName();
-        $shortAddress = str_replace("Abeille/", "", $eqLogic->getLogicalId());
-        $shortAddress = ($name == 'Ruche') ? "0000" : $shortAddress;
-        $X = max( 0, $eqLogic->getConfiguration('positionX'));
-        $Y = max( 0, $eqLogic->getConfiguration('positionY'));
-        
-        $json = $json . '"' . $shortAddress.'": { "name": "'.$name.'", "positionX": '.$X.', "positionY": '.$Y.' }, ';
+        if ( strpos($eqLogic->getLogicalId(),"Timer") > 1 ) {
+            // C'est un timer, je ne l envoie pas
+        }
+        else {
+            $name = $eqLogic->getName();
+            $shortAddress = str_replace("Abeille/", "", $eqLogic->getLogicalId());
+            $shortAddress = ($name == 'Ruche') ? "0000" : $shortAddress;
+            $X = max( 0, $eqLogic->getConfiguration('positionX'));
+            $Y = max( 0, $eqLogic->getConfiguration('positionY'));
+            
+            $json = $json . '"' . $shortAddress.'": { "name": "'.$name.'", "positionX": '.$X.', "positionY": '.$Y.' }, ';
+        }
     }
     
     $json = substr($json,0,-2) . ' }] ';

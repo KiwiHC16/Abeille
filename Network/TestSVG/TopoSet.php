c86c948c9946ff0bbf52502c6cf477dae1fb2667
KiwiHC16 <moulinb@free.fr>
2018-10-05 16:46:38 +0200

    Post method for big file to be tested #385

--- Network/TestSVG/TopoSet.php
+++ Network/TestSVG/TopoSet.php
@@ -2,8 +2,11 @@
     
     require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
     
-    $AbeilleTopoJSON = $_GET["TopoJSON"];
-    
+    echo "Php Begin - ";
+    // $AbeilleTopoJSON = $_GET["TopoJSON"];
+    // $AbeilleTopoJSON = $_POST["TopoJSON"];
+    $AbeilleTopoJSON = $_POST['TopoJSON'];
+    echo $AbeilleTopoJSON;
     // echo $AbeilleTopoJSON;
     
     // $AbeilleTopoJSON = '{"0000":{"name":"Ruche","x":333,"y":555,"color":"Red","positionDefined":"Yes","Type":"Coordinator"},"63d6":{"name":"IR 1","x":900,"y":500,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"7a8a":{"name":"IR 2","x":782.842712474619,"y":782.842712474619,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"030B":{"name":"IR 3","x":500,"y":900,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"0C9a":{"name":"Smoke","x":217.15728752538104,"y":782.842712474619,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"0A73":{"name":"Tp Carré","x":650,"y":370,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"7b0D":{"name":"Tp Rond","x":100,"y":500.00000000000006,"color":"Green","positionDefined":"Yes","Type":"Inconnu"},"3fcd":{"name":"Vibration","x":217.15728752538092,"y":217.15728752538104,"color":"Green","positionDefined":"Yes","Type":"Inconnu"}}';
@@ -40,7 +43,7 @@
        
     }
     
-     echo "Done";
+     echo " - Php End";
     
     ?>
 

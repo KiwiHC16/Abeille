b0081b8626e0b7179247077a403fd2c0d5253473
KiwiHC16 <moulinb@free.fr>
2018-10-05 10:41:05 +0200

    cosmetic

--- Network/TestSVG/upload.php
+++ Network/TestSVG/upload.php
@@ -1,9 +1,11 @@
 <?php
     $target_dir = "images/";
-    // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $target_file = $target_dir . "AbeilleLQI_MapData.png";
+    
     $uploadOk = 1;
+    
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -15,7 +17,8 @@
             $uploadOk = 0;
         }
     }
-    // Check if file already exists
+    
+    // Check if file already exists, in my case I don't let the choise I also write on the previous one.
     /*
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
@@ -27,12 +30,14 @@
         echo "Sorry, your file is too large.<br>";
         $uploadOk = 0;
     }
+    
     // Allow certain file formats
     // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"  && $imageFileType != "gif" ) {
     if( $imageFileType != "png" ) {
         echo "Sorry, only PNG files are allowed.<br>";
         $uploadOk = 0;
     }
+    
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.<br>";
@@ -44,6 +49,7 @@
             echo "Sorry, there was an error uploading your file.<br>";
         }
     }
+    echo "La page va se rafraichir dans 5 secondes avec le graph du reseau.</br>";
     
     header("Refresh:5; url=test.html");
     ?>

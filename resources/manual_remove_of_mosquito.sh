dbe00e7f49b3da5641d0e66f6b53613680ce105c
edgd1er <edgd1er@hotmail.com>
2018-10-06 16:33:47 +0200

    php: handle php v5 v7 v7.2 + code simplier

--- resources/manual_remove_of_mosquito.sh
+++ resources/manual_remove_of_mosquito.sh
@@ -1,28 +1,25 @@
 #! /bin/bash
 
-if [[ -d "/etc/php5/" ]]; then
-  if [[ -d "/etc/php5/cli/" && ! `cat /etc/php5/cli/php.ini | grep "mosquitto"` ]]; then
-  	sed -i '/extension=mosquitto.so/d' /etc/php5/cli/php.ini
+#functions
+removeMosquitto(){
 
-  fi
-  if [[ -d "/etc/php5/fpm/" && ! `cat /etc/php5/fpm/php.ini | grep "mosquitto"` ]]; then
-    sed -i '/extension=mosquitto.so/d' /etc/php5/fpm/php.ini
-    service php5-fpm restart
-  fi
-  if [[ -d "/etc/php5/apache2/" && ! `cat /etc/php5/apache2/php.ini | grep "mosquitto"` ]]; then
-    sed -i '/extension=mosquitto.so/d' /etc/php5/apache2/php.ini
-    service apache2 restart
-  fi
-else
-  if [[ -d "/etc/php/7.0/cli/" && `cat /etc/php/7.0/cli/php.ini | grep "mosquitto"` ]]; then
-    sed -i '/extension=mosquitto.so/d' /etc/php/7.0/cli/php.ini
-  fi
-  if [[ -d "/etc/php/7.0/fpm/" && `cat /etc/php/7.0/fpm/php.ini | grep "mosquitto"` ]]; then
-    sed -i '/extension=mosquitto.so/d' /etc/php/7.0/fpm/php.ini
-    service php5-fpm restart
-  fi
-  if [[ -d "/etc/php/7.0/apache2/" && `cat /etc/php/7.0/apache2/php.ini | grep "mosquitto"` ]]; then
-    sed -i '/extension=mosquitto.so/d' /etc/php/7.0/apache2/php.ini
-    service apache2 restart
-  fi
-fi
+    for phpdir in "${PHPETC}/cli" "${PHPETC}/fpm" "${PHPETC}/apache2"
+    do
+        if [[ -d "${phpdir}" ]]; then
+            sed -i '/extension=mosquitto.so/d' ${phpdir}/php.ini
+        fi
+    done
+    service php${PHPVER}-fpm restart >/dev/null 2>&1
+    service apache2 restart >/dev/null 2>&1
+}
+
+#Main
+PHPVER=""
+[[ -d "/etc/php5/" ]] && PHPVER=5 && PHPETC=/etc/php5
+[[ ! -z ${PHPVER} ]] && removeMosquitto
+PHPVER=""
+[[ -d "/etc/php/7.0" ]] && PHPVER=7.0 && PHPETC=/etc/php/7.0
+[[ ! -z ${PHPVER} ]] && removeMosquitto
+PHPVER=""
+[[ -d "/etc/php/7.2" ]] && PHPVER=7.2 && PHPETC=/etc/php/7.2
+[[ ! -z ${PHPVER} ]] && removeMosquitto

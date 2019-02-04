8d6b379168c54fb3c04bf889b9b21630395c87fb
KiwiHC16 <moulinb@free.fr>
2019-02-04 12:38:57 +0100

    remove tmp file left by mistake

--- resources/syncconf.sh
+++ resources/syncconf.sh
@@ -15,9 +15,11 @@ unzip master.zip
 
 cd /var/www/html/plugins/Abeille/tmp/Abeille-master
 mv images /var/www/html/plugins/Abeille
+chmod -R 755 /var/www/html/plugins/Abeille/images
 
 cd /var/www/html/plugins/Abeille/tmp/Abeille-master/core/config/
 mv devices /var/www/html/plugins/Abeille/core/config/
+chmod -R 755 /var/www/html/plugins/Abeille/core/config/devices
 
 cd /var/www/html/plugins/Abeille/
 rm -rf tmp > /dev/null 2>&1
--- tmp/Abeille-master/resources/AbeilleDeamon/includes/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $in = "/tmp/AbeilleDeamonInput";
-    $resourcePath=realpath(dirname(__FILE__).'/../../');
-    $WifiLink = "/tmp/zigate";
-?>

5fccf7d8d36213adc71760f3892815a48c984245
edgd1er <edgd1er@hotmail.com>
2019-03-14 08:26:08 +0100

    fix not existing IEEE value at the moment

--- core/class/Abeille.class.php
+++ core/class/Abeille.class.php
@@ -1985,13 +1985,13 @@
                 
                 // Je rejete les valeur null (arrive avec les equipement xiaomi qui envoie leur nom spontanement alors que l IEEE n est pas recue.
                 if (strlen($value)<2) {
-                    log::add( 'Abeille', 'debug', 'IEEE-Addr; =>'.$value.'<= ; IEEE non valable pour un equipement, valeur rejetée: '.$addr.": ".$IEEE." =>".$value."<=" );
+                    log::add( 'Abeille', 'debug', 'IEEE-Addr; =>'.$value.'<= ; IEEE non valable pour un equipement, valeur rejetée: '.$addr.": IEEE =>".$value."<=" );
                     return;
                 }
                 
                 // ffffffffffffffff remonte avec les mesures LQI si nouveau equipements.
                 if ($value == "ffffffffffffffff") {
-                    log::add( 'Abeille', 'debug', 'IEEE-Addr; =>'.$value.'<= ; IEEE non valable pour un equipement, valeur rejetée: '.$addr.": ".$IEEE." =>".$value."<=" );
+                    log::add( 'Abeille', 'debug', 'IEEE-Addr; =>'.$value.'<= ; IEEE non valable pour un equipement, valeur rejetée: '.$addr.": IEEE =>".$value."<=" );
                     return;
                 }
                 

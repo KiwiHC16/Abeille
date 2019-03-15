338428001dc4c7713651374d89ffaf4d0e1b4202
KiwiHC16 <moulinb@free.fr>
2019-03-15 15:30:59 +0100

    log

--- resources/AbeilleDeamon/AbeilleMQTTCmd.php
+++ resources/AbeilleDeamon/AbeilleMQTTCmd.php
@@ -48,8 +48,9 @@
             // deamonlog('debug', 'execTempoCmdAbeille - mqttMessageQueue - 0: '.$mqttMessage[0] );
             if ($mqttMessage[0]<$now) {
                 $client->publish( $mqttMessage[1], $mqttMessage[2], $qos);
+                deamonlog('debug', 'execTempoCmdAbeille - mqttMessageQueue - one less: '.json_encode($GLOBALS["mqttMessageQueue"][$key]) );
                 unset($GLOBALS["mqttMessageQueue"][$key]);
-                deamonlog('debug', 'execTempoCmdAbeille - mqttMessageQueue - one less: '.json_encode($GLOBALS["mqttMessageQueue"]) );
+                deamonlog('debug', 'execTempoCmdAbeille - mqttMessageQueue - Rest: '.json_encode($GLOBALS["mqttMessageQueue"]) );
             }
         }
         // deamonlog('debug', 'execTempoCmdAbeille - mqttMessageQueue - fin: '.json_encode($GLOBALS["mqttMessageQueue"]) );

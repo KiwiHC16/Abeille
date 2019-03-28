157e497e60bdfbbe4c2b92f5996223fbd8040584
KiwiHC16 <moulinb@free.fr>
2019-03-28 12:04:31 +0100

    Health on mobile (web app) #650

---
+++ desktop/modal/healthShort.php
@@ -0,0 +1,73 @@
+<?php
+    /* This file is part of Jeedom.
+     *
+     * Jeedom is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * Jeedom is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    require_once dirname(__FILE__).'/../../../../core/php/core.inc.php';
+    include_once(dirname(__FILE__).'/../../resources/AbeilleDeamon/lib/Tools.php');
+    /*
+    if (!isConnect('admin')) {
+        throw new Exception('401 Unauthorized');
+    }
+     */
+    $eqLogics = Abeille::byType('Abeille');
+?>
+
+<table class="table table-condensed tablesorter" id="table_healthAbeille">
+    <thead>
+    <tr>
+        <th>{{Module}}</th>
+        <th>{{Statut}}</th>
+        <th>{{Depuis (h)}}</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+        
+        foreach ($eqLogics as $eqLogic) {
+
+            // Module
+            echo "\n\n\n\n<tr>".'<td><a href="'.$eqLogic->getLinkToConfiguration().'" style="text-decoration: none;">'.$eqLogic->getHumanName(true).'</a></td>';
+
+            // Status
+            // Status Ok par defaut, apres on test et on met le status à la valeur voulue
+            $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">{{OK}}</span>';
+            if ( (time() - strtotime($eqLogic->getStatus('lastCommunication'))) > (60*$eqLogic->getTimeout()) ) {
+                 $status = '<span class="label label-warning" style="font-size : 1em; cursor : default;">Time Out Last Communication</span>';
+            }
+            if ( (time() - strtotime($eqLogic->getStatus('lastCommunication'))) > ((2*60*$eqLogic->getTimeout())) ) {
+                $status = '<span class="label label-danger" style="font-size : 1em; cursor : default;">Time Out Last Communication</span>';
+            }
+
+            if ($eqLogic->getStatus('state') == '-') {
+                $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">-</span>';
+            }
+            echo '<td>'.$status.'</td>';
+
+            // Depuis
+            $Depuis = '<span class="label label-info" style="font-size : 1em; cursor : default;">'.(floor((time() - strtotime($eqLogic->getStatus('lastCommunication'))) / 3600)).'</span>';
+            //if ($eqLogic->getStatus('state') == '-') { $Depuis = '<span class="label label-info" style="font-size : 1em; cursor : default;">-</span>'; }
+            echo '<td>'.$Depuis.'</td>';
+
+        }
+    ?>
+    </tbody>
+</table>
+
+<?php
+foreach ($IEEE_Table as $IEEE=>$IEEE_Device) {
+    if ($IEEE_Device>1) { echo "L'adresse $IEEE est dupliqué ce n'est pas normal. On ne doit avoir qu'un équipment par adresse IEEE</br>"; }
+}
+    ?>

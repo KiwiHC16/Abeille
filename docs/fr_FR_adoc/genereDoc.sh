8e61f74f7031e2c26b92c56bec6d8c839750cec4
KiwiHC16 <moulinb@free.fr>
2019-04-05 17:00:16 +0200

    Ajout de la doc en local (bypaas le remove jeedom)

--- docs/fr_FR_adoc/genereDoc.sh
+++ docs/fr_FR_adoc/genereDoc.sh
@@ -7,5 +7,6 @@ php listeCompatibilite.php 1 > listeCompatibilite.adoc
 asciidoc -n -a icons -a badge listeCompatibilite.adoc
 rm listeCompatibilite.adoc
 mv listeCompatibilite.html ../../../docs/fr_FR
+rm -f ../../../docs_locale/fr_FR/*
+cp ../../../docs/fr_FR/* ../../../docs_locale/fr_FR/*
 cd -
-

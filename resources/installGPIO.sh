c340d507852b83f8ccb0d1a1b69ad1dc21a6aa17
www-data <www-data@sms>
2019-04-23 15:46:20 +0000

    Split de l installation de wiringPi et de la conf port serie

--- resources/installGPIO.sh
+++ resources/installGPIO.sh
@@ -1,51 +1,32 @@
 echo "Lancement de l installation de GPIO"
 
+echo "Mise a jour du systeme Raspbian"
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+echo "installation de git"
 sudo apt-get -y install git-core
+
+echo "installation de make"
 sudo apt-get -y install make
+
+echo "installation de gcc"
 sudo apt-get -y install gcc
 
+echo "Recupeartion de wiringPi"
 cd /var/www/html/plugins/Abeille/
 mkdir tmp
 cd tmp
 git clone git://git.drogon.net/wiringPi
+
+echo "Compilation de wiringPi"
 cd wiringPi
 ./build
 cd /var/www/html/plugins/Abeille/
 rm -rf tmp
 
+echo "Verification que wiringPi est installé."
 gpio -v
 gpio readall
-
-DATE_WITH_TIME=`date +"%Y%m%d-%H%M%S"`
-
-sudo cp /boot/config.txt /boot/config.txt.$DATE_WITH_TIME
-
-sudo su - << END
-sed "/^enable_uart=1/d" /boot/config.txt > /boot/config.txt.new
-echo "" >> /boot/config.txt.new
-echo "enable_uart=1" >> /boot/config.txt.new
-mv /boot/config.txt.new /boot/config.txt
-END
-
-echo "RPI2"
-sudo systemctl stop serial-getty@ttyAMA0.service
-sudo systemctl disable serial-getty@ttyAMA0.service
-
-echo "RPI3"
-sudo systemctl stop serial-getty@ttyS0.service
-sudo systemctl disable serial-getty@ttyS0.service
-
-
-sudo cp /boot/cmdline.txt /boot/cmdline.txt.$DATE_WITH_TIME
-
-sudo su - << END2
-sed -e "s/console=serial[a-zA-Z]*[0-9]*,115200 //" /boot/cmdline.txt > /boot/cmdline.txt.new
-mv /boot/cmdline.txt.new /boot/cmdline.txt
-END2
-
-echo "Vous devez redemarrer votre RPI"
-sudo reboot
 
 echo "Fin"

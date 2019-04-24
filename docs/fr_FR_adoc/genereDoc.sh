2123fbd597c89f6428ab08a08547e6301b037025
KiwiHC16 <moulinb@free.fr>
2019-04-24 17:35:19 +0200

    Update of the documentation with last PiZiGate changes.

--- docs/fr_FR_adoc/genereDoc.sh
+++ docs/fr_FR_adoc/genereDoc.sh
@@ -1,12 +1,23 @@
+echo "========================"
+echo "Processing doc files ..."
+echo "========================"
+rm ../../docs_locale/fr_FR/*.html
+rm ../fr_FR/*.html
 for f in *.adoc; do echo "Processing $f file.."; asciidoc -n -a icons -a badge $f; done
+cp *.html ../../docs_locale/fr_FR/
 mv *.html ../fr_FR/
 
+echo "======================================"
 echo "Processing listeCompatibilite file ..."
+echo "======================================"
+echo "Generating listeCompatibilite.adoc file..."
 cd ../../core/config/devices
 php listeCompatibilite.php 1 > listeCompatibilite.adoc
 asciidoc -n -a icons -a badge listeCompatibilite.adoc
 rm listeCompatibilite.adoc
+cp listeCompatibilite.html ../../../docs_locale/fr_FR
 mv listeCompatibilite.html ../../../docs/fr_FR
-rm -f ../../../docs_locale/fr_FR/*
-cp ../../../docs/fr_FR/* ../../../docs_locale/fr_FR/*
-cd -
+
+echo "========"
+echo "All done"
+echo "========"

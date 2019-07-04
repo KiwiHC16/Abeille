b5aabdf447e3c1cd197e991322345f3cf450d211
KiwiHC16 <moulinb@free.fr>
2019-07-04 14:28:47 +0200

    Sémaphore downlink uplink #740: basic test

---
+++ core/class/test/Proc2.php
@@ -0,0 +1,21 @@
+<?php
+
+        $sema = sem_get( 1234, 1 );
+        
+        if ( sem_acquire( $sema, false ) ) {
+            echo "Got the semaphore\n";
+        }
+        else {
+            echo "Can't get the semaphore\n";
+        }
+        
+        if ( sem_release( $sema ) ) {
+            echo "Released the semaphore\n";
+        }
+        else {
+            echo "Can't release the semaphore\n";
+        }
+
+    
+    ?>
+

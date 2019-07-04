bcb11ce07f6305751810be81429714fed29cf420
KiwiHC16 <moulinb@free.fr>
2019-07-04 13:52:03 +0200

    reviewed compatibility script for Sphynx doc.

--- core/config/devices/listeCompatibilite.php
+++ core/config/devices/listeCompatibilite.php
@@ -1,16 +1,15 @@
 <?php
     // Pour mettre a jour la doc github:
     // php listeCompatibilite.php 1 > ../../../Documentation/040_Compatibilite.adoc
-
     
     function equipementHtml( $resultIcone, $resultRaw, $result) {
-    //---------------------------------------------------------------------
-    echo "<h1>Equipments</h1>";
-
-    echo "<table>";
-    sort( $resultIcone );
-    foreach ( $resultIcone as $values ) echo '<tr><td>'.$values['name'].'</td><td><img src="/plugins/Abeille/images/node_'.$values['icone'].'.png" width="100" height="100"></td></tr>'."\n";
-    echo "</table>";
+        
+        echo "<h1>Equipments</h1>";
+        
+        echo "<table>";
+        sort( $resultIcone );
+        foreach ( $resultIcone as $values ) echo '<tr><td>'.$values['name'].'</td><td><img src="/plugins/Abeille/images/node_'.$values['icone'].'.png" width="100" height="100"></td></tr>'."\n";
+        echo "</table>";
         
         //---------------------------------------------------------------------
         echo "<h1>Equipements et fonctions associées</h1>";
@@ -30,26 +29,41 @@
     }
     
     function equipementAdoc( $resultIcone, $resultRaw, $result) {
-    echo '= Compatibility'."\n";
-    echo 'KiwiHC16'."\n";
-    echo ':toc2:'."\n";
-    echo ':toclevels: 4'."\n";
-    echo ':toc-title: Table des matières'."\n";
-    echo ':imagesdir: ../../images'."\n";
-    echo ':iconsdir: ../images/icons'."\n";
-    echo "\n";
-    echo '== Home'."\n";
-    echo ''."\n";
-    echo 'Retour au link:index.html[document principal].'."\n";
-    echo ''."\n";
-    echo '== Liste'."\n";
-    echo ''."\n";
+        echo '= Compatibility'."\n";
+        echo 'KiwiHC16'."\n";
+        echo ':toc2:'."\n";
+        echo ':toclevels: 4'."\n";
+        echo ':toc-title: Table des matières'."\n";
+        echo ':imagesdir: ../../images'."\n";
+        echo ':iconsdir: ../images/icons'."\n";
+        echo "\n";
+        echo '== Home'."\n";
+        echo ''."\n";
+        echo 'Retour au link:index.html[document principal].'."\n";
+        echo ''."\n";
+        echo '== Liste'."\n";
+        echo ''."\n";
         
-    echo '[cols="<,^"]'."\n";
-    echo "|======="."\n";;
-    foreach ( $resultIcone as $values ) echo '| '.$values['name'].'| image:node_'.$values['icone'].'.png[height=200,width=200]'."\n";
-    echo "|======="."\n";;
+        echo '[cols="<,^"]'."\n";
+        echo "|======="."\n";;
+        foreach ( $resultIcone as $values ) echo '| '.$values['name'].'| image:node_'.$values['icone'].'.png[height=200,width=200]'."\n";
+        echo "|======="."\n";;
     }
+    
+    function equipementRst( $resultIcone, $resultRaw, $result) {
+        
+        echo "*********************************\n";
+        echo "Liste des équipements compatibles\n";
+        echo "*********************************\n";
+        echo "\n\n\n";
+        
+        foreach ( $resultIcone as $values ) echo "\n\n.. image:: imagesDevices/node_".$values['icone'].".png\n   :width: 200px\n\n".$values['name']."\n";
+        
+    }
+    
+    //----------------------------------------------------------------------------------------------------
+    // Main
+    //----------------------------------------------------------------------------------------------------
     
     // Recupere les info.
     foreach ( glob( '*/*.json') as $file ) {
@@ -67,8 +81,14 @@
         }
     }
     
+    // Met en forme.
     if (isset($argv[1])) {
-        equipementAdoc( $resultIcone, $resultRaw, $result);
+        if ( $argv[1] == "adoc" ) {
+            equipementAdoc( $resultIcone, $resultRaw, $result);
+        }
+        if ( $argv[1] == "rst" ) {
+            equipementRst( $resultIcone, $resultRaw, $result);
+        }
     } else {
         equipementHtml( $resultIcone, $resultRaw, $result);
     }

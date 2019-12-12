0c7c791225b804167fa8ff63167643cfc88258f5
KiwiHC16 <moulinb@free.fr>
2019-12-12 16:27:35 +0100

    Formatting to read easily

--- desktop/modal/network.php
+++ desktop/modal/network.php
@@ -1,40 +1,54 @@
 <?php
-
-if (!isConnect('admin')) {
-    throw new Exception('401 Unauthorized');
-}
-
-$startTime = config::byKey('lastDeamonLaunchTime', 'Abeille', '{{Jamais lancé}}');
-$usbPath = config::byKey('', 'Abeille', '{{Jamais lancé}}');
-$status = "<i class=\"fa fa-circle fa-lg rediconcolor\"></i> Plugin désactivé et démon non configuré";
-if (config::byKey('active', 'Abeille', '0') == 1) {
-    if (Abeille::deamon_info()['state'] == 'ok') {
-        $status = "<i class=\"fa fa-circle fa-lg greeniconcolor\"></i> Plugin activé et démon configuré";
-    } else {
-        $status = "<i class=\"fa fa-circle fa-lg rediconcolor\"></i> Plugin activé mais démon non configué";
-    }
-
-}
-
-
-$neighbors = "";
-$color = "greeniconcolor";
-$usbPath = config::byKey('AbeilleSerialPort', 'Abeille');
-$eqLogics = eqLogic::byType('Abeille');
-$nodesCount = count($eqLogics);
-$nodes = array();
-foreach ($eqLogics as $eqLogic) {
-    $neighbors .= $eqLogic->getName() . ", ";
-    $nodes[str_replace("/", "x", $eqLogic->getLogicalId())] = $eqLogic->getId();
-}
-$neighbors = substr($neighbors, 0, -2);
-
-$processes = Abeille::deamon_info();
-$color = ($processes['nbProcess'] == $processes['nbProcessExpected']) ? "greeniconcolor" : "rediconcolor";
-$nbDaemons = "<i class=\"fa fa-circle fa-lg " . $color . "\"></i> " . $processes['nbProcess'] . "/".$processes['nbProcessExpected'];
-
     
-sendVarToJS('nodesFromJeedom', $nodes);
+    if (!isConnect('admin')) {
+        throw new Exception('401 Unauthorized');
+    }
+    
+    // Last Demon start time
+    $startTime = config::byKey('lastDeamonLaunchTime', 'Abeille', '{{Demon Jamais lancé}}');
+    
+    // Status demon
+    $status = "<i class=\"fa fa-circle fa-lg rediconcolor\"></i> Plugin désactivé et démon non configuré";
+    if (config::byKey('active', 'Abeille', '0') == 1) {
+        if (Abeille::deamon_info()['state'] == 'ok') {
+            $status = "<i class=\"fa fa-circle fa-lg greeniconcolor\"></i> Plugin activé et démon configuré";
+        } else {
+            $status = "<i class=\"fa fa-circle fa-lg rediconcolor\"></i> Plugin activé mais démon non configué";
+        }
+    }
+    
+    // Chemin vers les zigates
+    $usbPath  = config::byKey('AbeilleSerialPort',  'Abeille', '{{Inconnu}}');
+    $usbPath2 = config::byKey('AbeilleSerialPort2', 'Abeille', '{{Inconnu}}');
+    $usbPath3 = config::byKey('AbeilleSerialPort3', 'Abeille', '{{Inconnu}}');
+    $usbPath4 = config::byKey('AbeilleSerialPort4', 'Abeille', '{{Inconnu}}');
+    $usbPath5 = config::byKey('AbeilleSerialPort5', 'Abeille', '{{Inconnu}}');
+    $eqLogics = eqLogic::byType('Abeille');
+    
+    // Node Count
+    $nodesCount = count($eqLogics);
+    $timerCount = 0;
+    foreach ($eqLogics as $eqLogic) {
+        if (preg_match("(Timer)", $eqLogic->getLogicalId())) $timerCount++;
+    }
+    
+    // Liste des noeuds
+    $neighbors = "";
+    $nodes = array();
+    foreach ($eqLogics as $eqLogic) {
+        $neighbors .= $eqLogic->getName() . ", ";
+        $nodes[str_replace("/", "x", $eqLogic->getLogicalId())] = $eqLogic->getId();
+    }
+    $neighbors = substr($neighbors, 0, -2); // enleve la virgule et l espace en fin de chaine
+    
+    // Nombre de process actifs
+    $processes = Abeille::deamon_info();
+    $color = "greeniconcolor";
+    $color = ($processes['nbProcess'] == $processes['nbProcessExpected']) ? "greeniconcolor" : "rediconcolor";
+    $nbDaemons = "<i class=\"fa fa-circle fa-lg " . $color . "\"></i> " . $processes['nbProcess'] . "/".$processes['nbProcessExpected'];
+    
+    
+    sendVarToJS('nodesFromJeedom', $nodes);
 
 ?>
 <style>
@@ -115,14 +129,9 @@ sendVarToJS('nodesFromJeedom', $nodes);
         <div id="content">
             <ul id="tabs_network" class="nav nav-tabs" data-tabs="tabs">
                 <li class="active"> <a href="#summary_network"                          data-toggle="tab"> <i class="fa fa-tachometer">  </i> {{Résumé}}                </a></li>
-                <li>                <a href="#actions_network"    style="display:none;" data-toggle="tab"> <i class="fa fa-sliders">     </i> {{Actions}}               </a></li>
-                <li>                <a href="#statistics_network" style="display:none;" data-toggle="tab"> <i class="fa fa-bar-chart">   </i> {{Statistiques}}          </a></li>
                 <li id="tab_graph"> <a href="#graph_network"                            data-toggle="tab"> <i class="fa fa-picture-o">   </i> {{Graphique du réseau}}   </a></li>
                 <li id="tab_route"> <a href="#route_network"                            data-toggle="tab"> <i class="fa fa-table">       </i> {{Table des noeuds}}      </a></li>
             </ul>
-
-
-
 
             <div id="network-tab-content" class="tab-content">
                 <div class="tab-pane active" id="summary_network">
@@ -130,46 +139,24 @@ sendVarToJS('nodesFromJeedom', $nodes);
                     <div class="panel panel-primary">
                         <div class="panel-heading"><h4 class="panel-title">{{Informations}}</h4></div>
                         <div class="panel-body">
-                            <p>{{Réseau démarré le}} <span class="zigBNetworkAttr label label-default"
-                                                           style="font-size : 1em;"
-                                                           data-l1key="startTime"><?php echo $startTime ?></span> <span
-                                        class="zigBNetworkAttr label label-default" data-l1key="awakedDelay"
-                                        style="font-size : 1em;"></span></p>
-                            <p>{{Le réseau contient}} <b><span class="zigBNetworkAttr"
-                                                               data-l1key="nodesCount"></span><?php echo $nodesCount ?>
-                                </b>
-                                {{noeuds}}</p>
-                            <p>{{Voisins :}}<span class="zigBNetworkAttr label-default" data-l1key="neighbors"
-                                                  style="font-size : 1em;"><?php echo $neighbors ?></span></p>
+                            <p>{{Réseau démarré le}} <span class="zigBNetworkAttr label label-default" style="font-size : 1em;" data-l1key="startTime"><?php echo $startTime ?></span> <span class="zigBNetworkAttr label label-default" data-l1key="awakedDelay" style="font-size : 1em;"></span></p>
+                            <p>{{Le réseau contient}} <b><span class="zigBNetworkAttr" data-l1key="nodesCount"></span><?php echo $nodesCount ?> </b> {{noeuds dont }} <?php echo $nodesCount-$timerCount ?> {{noeuds zigbee (zigate(s) incluse(s)) et }} <?php echo $timerCount ?> {{timers}}</p>
+                            <p>{{Voisins :}}<span class="zigBNetworkAttr label-default" data-l1key="neighbors" style="font-size : 1em;"><?php echo $neighbors ?></span></p>
                         </div>
                     </div>
+
                     <div class="panel panel-primary">
                         <div class="panel-heading"><h4 class="panel-title">{{Etat}}</h4></div>
                         <div class="panel-body">
-                            <p><span class="zigBNetworkAttr" data-l1key="state"></span> {{Etat actuel :}} <span
-                                        class="zigBNetworkAttr label label-default" data-l1key="stateDescription"
-                                        style="font-size : 1em;"><?php echo $status ?></span></p>
+                            <p><span class="zigBNetworkAttr" data-l1key="state"></span> {{Etat actuel :}} <span class="zigBNetworkAttr label label-default" data-l1key="stateDescription" style="font-size : 1em;"><?php echo $status ?></span></p>
                         </div>
                     </div>
-                    <div class="panel panel-primary" style="display:none;">
-                        <div class="panel-heading"><h4 class="panel-title">{{Capacités}}</h4></div>
-                        <div class="panel-body">
-                            <lu style="font-size : 1em;"><span class="zigBNetworkAttr" data-l1key="node_capabilities"
-                                                               style="font-size : 1em;"></span></lu>
-                        </div>
-                    </div>
+
                     <div class="panel panel-primary">
                         <div class="panel-heading"><h4 class="panel-title">{{Système}}</h4></div>
                         <div class="panel-body">
-                            <p>{{Chemin du contrôleur Zigbee :}} <span class="zigBNetworkAttr label label-default"
-                                                                       data-l1key=""
-                                                                       style="font-size : 1em;"><?php echo $usbPath ?></span>
-                            </p>
-                            <p>{{Nombre de démons lancés :}} <span class="zigBNetworkAttr label label-default"
-                                                                   data-l1key=""
-                                                                   style="font-size : 1em;"><?php echo $nbDaemons ?></span>
-                            </p>
-
+                            <p>{{Chemin du contrôleur Zigbee :}} <span class="zigBNetworkAttr label label-default" data-l1key="" style="font-size : 1em;"><?php echo $usbPath.", ".$usbPath2.", ".$usbPath3.", ".$usbPath4.", ".$usbPath5 ?></span> </p>
+                            <p>{{Nombre de démons lancés :}} <span class="zigBNetworkAttr label label-default" data-l1key="" style="font-size : 1em;"><?php echo $nbDaemons ?></span> </p>
                         </div>
                     </div>
                 </div>
@@ -254,35 +241,6 @@ sendVarToJS('nodesFromJeedom', $nodes);
                 </div>
 
 
-
-
-                <div class="tab-pane" id="actions_network">
-                    <table class="table">
-
-                    </table>
-                </div>
-
-
-
-
-                <div class="tab-pane" id="statistics_network">
-                    <table class="table table-condensed table-striped">
-                        <tr>
-                            <td><b>{{Nombre d'émissions lues :}}</b></td>
-                            <td><span class="zigBNetworkAttr" data-l1key="controllerStatistics"
-                                      data-l2key="broadcastReadCnt"></span></td>
-                        </tr>
-                        <tr>
-                            <td><b>{{Nombre d'émissions envoyées :}}</b></td>
-                            <td><span class="zigBNetworkAttr" data-l1key="controllerStatistics"
-                                      data-l2key="broadcastWriteCnt"></span></td>
-                        </tr>
-                    </table>
-                </div>
-                                
-                                
-                                
-                                
             </div>
         </div>
     </div>

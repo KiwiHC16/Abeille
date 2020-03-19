eaafa3fed6ee0cafaa7639432f4136430290f5fa
KiwiHC16 <moulinb@free.fr>
2020-03-19 15:57:29 +0100

    Simplified the code using json_encode, adding object name,...

--- Network/TestSVG/TopoGet.php
+++ Network/TestSVG/TopoGet.php
@@ -2,25 +2,22 @@
     
     require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
     
-    $json = "[{ ";
+    $table = array();
     
     $eqLogics = eqLogic::byType('Abeille');
     foreach ($eqLogics as $eqLogic) {
-        $logicalId = $eqLogic->getId();
-        $name = $eqLogic->getName();
-        //$shortAddress = str_replace("Abeille/", "", $eqLogic->getLogicalId());
-        $id = $eqLogic->getLogicalId();
-        // $shortAddress = ($name == 'Ruche') ? "0000" : $shortAddress;
-        $X = max( 0, $eqLogic->getConfiguration('positionX'));
-        $Y = max( 0, $eqLogic->getConfiguration('positionY'));
+        $device['objectName']   = $eqLogic->getObject()->getName();
+        $device['name']         = $eqLogic->getName();
+        $device['logicalId']    = $eqLogic->getLogicalId();
+        $device['id']           = $eqLogic->getId();
+        $device['X']            = max( 0, $eqLogic->getConfiguration('positionX'));
+        $device['Y']            = max( 0, $eqLogic->getConfiguration('positionY'));
         
-        $json = $json . '"' . $id.'": { "name": "'.$name.'", "positionX": '.$X.', "positionY": '.$Y.', "logicalId": "'.$logicalId.'" }, ';
+        $table[$eqLogic->getLogicalId()] = $device;
+        unset( $device );
     }
     
-    $json = substr($json,0,-2) . ' }] ';
-    
-    echo $json;
-    
+    echo json_encode($table);;
     
     ?>
 

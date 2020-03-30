134ccbeec5bb078f114bd5d133b14b7c0d55f756
KiwiHC16 <moulinb@free.fr>
2020-03-30 17:59:20 +0200

    Print also in log and not command in web browser, too heavy.

--- desktop/modal/getCompleteAbeilleConf.php
+++ desktop/modal/getCompleteAbeilleConf.php
@@ -2,31 +2,50 @@
     
     require_once dirname(__FILE__).'/../../../../core/php/core.inc.php';
     include_once(dirname(__FILE__).'/../../resources/AbeilleDeamon/lib/Tools.php');
-    
+    /*
     if (!isConnect('admin')) {
         throw new Exception('401 Unauthorized');
     }
-    
+    */
     $eqLogics = Abeille::byType('Abeille');
     
     global $CONFIG;
     
-    function requestAndPrint( $link, $sql, $title) {
-        echo "<br>\n<br>\n";
-        echo "-----------------------<br>\n";
-        echo $title."<br>\n";
-        echo "-----------------------<br>\n";
+    function requestAndPrint( $link, $sql, $title, $printModal, $printFile) {
+        if ( $printModal ) {
+            echo "<br>\n<br>\n";
+            echo "-----------------------<br>\n";
+            echo $title."<br>\n";
+            echo "-----------------------<br>\n";
+            
+            echo "{";
+        }
+        if ( $printFile ) {
+            log::add( 'AbeilleDbConf', 'info', '');
+            log::add( 'AbeilleDbConf', 'info', '-----------------------');
+            log::add( 'AbeilleDbConf', 'info', $title);
+            log::add( 'AbeilleDbConf', 'info', '-----------------------');
+            $i=0;
+            log::add( 'AbeilleDbConf', 'info', '{');
+        }
+        
         $i=0;
-        echo "{";
         if ($result = mysqli_query($link, $sql)) {
             while ($row = $result->fetch_assoc()) {
-                if ($i==0) { echo '"'.$i.'":'.json_encode($row); }
-                else { echo ',"'.$i.'":'.json_encode($row); }
+                if ( $printModal ) {
+                    if ($i==0) { echo '"'.$i.'":'.json_encode($row); }
+                    else { echo ',"'.$i.'":'.json_encode($row); }
+                }
+                if ($printFile) {
+                    if ($i==0) {log::add( 'AbeilleDbConf', 'info', '"'.$i.'":'.json_encode($row)); }
+                    else { log::add( 'AbeilleDbConf', 'info', ',"'.$i.'":'.json_encode($row)); }
+                }
                 $i++;
             }
             mysqli_free_result($result);
         }
-        echo "}";
+        if ( $printModal ) { echo "}"; }
+        if ( $printFile ) { log::add( 'AbeilleDbConf', 'info', '}'); }
     }
     
     echo "Dans cette page nous allons extraire toutes les informations necessaires a l analyse du plugin.<br> <br>\n";
@@ -39,11 +58,11 @@
         exit();
     }
     
-    requestAndPrint($link, "SELECT * FROM `update` WHERE `name` = 'Abeille'", "Version");
-    requestAndPrint($link, "select * from config where plugin = 'Abeille'", "Configuration du plugin");
-    requestAndPrint($link, "SELECT * FROM `cron` WHERE `class` = 'Abeille'", "Liste des cron");
-    requestAndPrint($link, "select * from eqLogic where eqType_name = 'Abeille'", "Liste des abeilles");
-    requestAndPrint($link, "SELECT * FROM cmd WHERE eqType = 'Abeille'", "Liste des commandes");
+    requestAndPrint($link, "SELECT * FROM `update` WHERE `name` = 'Abeille'", "Version",                1, 1);
+    requestAndPrint($link, "select * from config where plugin = 'Abeille'", "Configuration du plugin",  1, 1);
+    requestAndPrint($link, "SELECT * FROM `cron` WHERE `class` = 'Abeille'", "Liste des cron",          1, 1);
+    requestAndPrint($link, "select * from eqLogic where eqType_name = 'Abeille'", "Liste des abeilles", 1, 1);
+    requestAndPrint($link, "SELECT * FROM cmd WHERE eqType = 'Abeille'", "Liste des commandes",         0, 1);
     
     mysqli_close($link);
     

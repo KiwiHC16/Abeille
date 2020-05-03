f23511c58bac062b2c85a6b03aec4729b2787d50
W1W1-M <w1w1_m@icloud.com>
2020-05-03 20:17:41 +0200

    Maj texte pour traduction

--- desktop/modal/listeCompatibilite.php
+++ desktop/modal/listeCompatibilite.php
@@ -1,33 +1,33 @@
 <?php
     // Pour mettre a jour la doc github:
     // php listeCompatibilite.php rst
-    
+
     function equipementHtml( $resultIcone, $resultRaw, $result) {
-        
-        echo "<h1>Equipments</h1>";
-        
+
+        echo "<h1>{{Equipments}}</h1>";
+
         echo "<table>";
         sort( $resultIcone );
         foreach ( $resultIcone as $values ) echo '<tr><td>'.$values['name'].'</td><td><img src="/plugins/Abeille/images/node_'.$values['icone'].'.png" width="100" height="100"></td></tr>'."\n";
         echo "</table>";
-        
+
         //---------------------------------------------------------------------
-        echo "<h1>Equipements et fonctions associées</h1>";
+        echo "<h1>{{Equipements et fonctions associées}}</h1>";
         echo "<table>\n";
-        echo "<tr><td>Nom</td><td>Nom Zigbee</td><td>Fonction</td></tr>\n";
-        
+        echo "<tr><td>{{Nom}}</td><td>{{Nom Zigbee}}</td><td>{{Fonction}}</td></tr>\n";
+
         sort( $result );
         foreach ( $result as $line ) echo $line."\n";
         echo "</table>\n";
-        
+
         //---------------------------------------------------------------------
-        echo "<h1>Fonctions utilisées</h1>";
+        echo "<h1>{{Fonctions utilisées}}</h1>";
         $includeList = array_column( $resultRaw, 'fonction');
         $includeList = array_unique($includeList);
         sort( $includeList );
         foreach ( $includeList as $value ) echo $value."<br>\n";
     }
-    
+
     function equipementAdoc( $resultIcone, $resultRaw, $result) {
         echo '= Compatibility'."\n";
         echo 'KiwiHC16'."\n";
@@ -43,28 +43,28 @@
         echo ''."\n";
         echo '== Liste'."\n";
         echo ''."\n";
-        
+
         echo '[cols="<,^"]'."\n";
         echo "|======="."\n";;
         foreach ( $resultIcone as $values ) echo '| '.$values['name'].'| image:node_'.$values['icone'].'.png[height=200,width=200]'."\n";
         echo "|======="."\n";;
     }
-    
+
     function equipementRst( $resultIcone, $resultRaw, $result) {
-        
+
         echo "*********************************\n";
         echo "Liste des équipements compatibles\n";
         echo "*********************************\n";
         echo "\n\n\n";
-        
+
         foreach ( $resultIcone as $values ) echo "\n\n.. image:: imagesDevices/node_".$values['icone'].".png\n   :width: 200px\n\n".$values['name']."\n";
-        
+
     }
-    
+
     //----------------------------------------------------------------------------------------------------
     // Main
     //----------------------------------------------------------------------------------------------------
-    
+
     // Recupere les info.
     foreach ( glob( '/var/www/html/plugins/Abeille/core/config/devices/*/*.json') as $file ) {
         // echo $file."\n";
@@ -72,13 +72,13 @@
         if ( basename(dirname($file)) == "Template" ) {
             continue;
         }
-        
+
         $name = basename($file, ".json");
         // echo $name."\n";
         $contentJSON = file_get_contents( $file );
         $content = JSON_decode( $contentJSON, true );
         $resultIcone[] = array( 'name'=>$content[$name]["nameJeedom"], 'icone'=>$content[$name]["configuration"]["icone"] );
-        
+
         /*
         echo "File:\n";
         var_dump( $file );
@@ -87,13 +87,13 @@
         echo "resultIcone:\n";
         var_dump( $resultIcone );
         */
-        
+
         foreach ( $content[$name]['Commandes'] as $include ) {
             $resultRaw[] = array( 'name'=>$content[$name]["nameJeedom"], 'nameZigbee'=>$name, 'fonction'=>$include );
             $result[] = "<tr><td>".$content[$name]["nameJeedom"]."</td><td>".$name."</td><td>".$include."</td></tr>";
         }
     }
-    
+
     // Met en forme.
     if (isset($argv[1])) {
         if ( $argv[1] == "adoc" ) {
@@ -105,5 +105,5 @@
     } else {
         equipementHtml( $resultIcone, $resultRaw, $result);
     }
-    
+
     ?>

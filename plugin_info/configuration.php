918d02ad5ad1d4e7f5f1c8586fb64be18e6d776b
W1W1-M <w1w1_m@icloud.com>
2020-05-05 21:52:05 +0200

    Maj texte pour traduction

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -35,7 +35,7 @@
 
 
             <div>
-                <p><i> En passant votre souris sur les titres des champs, vous pouvez obtenir des informations specifiques sur chaque champs.</i></p>
+                <p><i>{{En passant votre souris sur les titres des champs, vous pouvez obtenir des informations specifiques sur chaque champs.}}</i></p>
                 <div class="form-group">
                     <label class="col-lg-4 control-label" data-toggle="tooltip" title="Version Abeille.">{{Version Abeille : }}</label>
                     <div class="col-lg-4">
@@ -49,15 +49,15 @@
 
             <div id="Connection">
                 <div>
-                    <p><i>La ZiGate possede trois types possibles de modules pour etre connecté au système Jeedom.</i></p>
-                    <p><i>Un module USB / PiZigate(GPIO) ou un module Wifi. Si vous êtes en wifi choisissez WIFI ici et indiquez dans le champ IP:Port les informations du module WIFI.</i></p>
-                    <p><i>Si vous êtes en USB, choisissez le port USB du type ttyUSBx sur lequel la ZiGate est branchée. Pour PiZiGate choisissez le port série /dev/ttyS0.</i></p>
-                    <p><i>Il faut au minimum une zigate sur le premier port.</i></p>
-                    <p><i>Bien mettre les zigates non utilisées sur la valeur Aucun sinon le demon risque de ne pas demarrer.</i></p>
+                    <p><i>{{La ZiGate possede trois types possibles de modules pour etre connecté au système Jeedom.}}</i></p>
+                    <p><i>{{Un module USB / PiZigate(GPIO) ou un module Wifi. Si vous êtes en wifi choisissez WIFI ici et indiquez dans le champ IP:Port les informations du module WIFI.}}</i></p>
+                    <p><i>{{Si vous êtes en USB, choisissez le port USB du type ttyUSBx sur lequel la ZiGate est branchée. Pour PiZiGate choisissez le port série /dev/ttyS0.}}</i></p>
+                    <p><i>{{Il faut au minimum une zigate sur le premier port.}}</i></p>
+                    <p><i>{{Bien mettre les zigates non utilisées sur la valeur Aucun sinon le demon risque de ne pas demarrer.}}</i></p>
                 </div>
 
             <div class="form-group">
-                <label class="col-lg-4 control-label" data-toggle="tooltip" title="Nombre de zigate (si vous changez cette valeur, sauvegarder et recharger la page.">{{Nombre de zigate : }}</label>
+                <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Nombre de zigate (si vous changez cette valeur, sauvegarder et recharger la page.}}">{{Nombre de zigate : }}</label>
                 <div class="col-sm-4">
                     <select class="configKey form-control col-sm-12" data-l1key="zigateNb">
                     <?php
@@ -75,12 +75,12 @@
                 <div class="form-group"><label class="col-lg-4 control-label">-----</label>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Nom donné à la ruche qui controle ce réseau zigbee">{{Nom du réseau zigbee : }}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Nom donné à la ruche qui controle ce réseau zigbee}}">{{Nom du réseau zigbee : }}</label>
                     <?php if (Abeille::byLogicalId( 'Abeille'.$i.'/Ruche', 'Abeille')) {echo Abeille::byLogicalId( 'Abeille'.$i.'/Ruche', 'Abeille')->getHumanName();} ?>
                 </div>
 
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Choisissez le port serie ou le mode WIFI">{{Abeille Serial Port : }}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Choisissez le port serie ou le mode WIFI}}">{{Abeille Serial Port : }}</label>
                     <div class="col-lg-4">
                             <?php
                                 echo '<select class="configKey form-control col-sm-12" data-l1key="AbeilleSerialPort'.$i.'">';
@@ -100,14 +100,14 @@
                 </div>
 
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Adresse IP de la zigate ainsi que le port (IP:Port(9999/23)). 9999 est le port du module wifi zigate par dafaut, mettre 23 si vous utilisez ESP-Link.">{{IP (IP:Port) de Zigate Wifi : }}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Adresse IP de la zigate ainsi que le port (IP:Port(9999/23)). 9999 est le port du module wifi zigate par dafaut, mettre 23 si vous utilisez ESP-Link.}}">{{IP (IP:Port) de Zigate Wifi : }}</label>
                     <div class="col-sm-4">
                     <?php echo '<input class="configKey form-control" data-l1key="IpWifiZigate'.$i.'" style="margin-top:5px" placeholder="192.168.4.1:9999"/>'; ?>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Activer ou desactiver l utilisation de cette zigate.">{{Activer}}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Activer ou desactiver l'utilisation de cette zigate.}}">{{Activer}}</label>
                     <div class="col-lg-4">
                         <?php echo '<select class="configKey form-control" col-sm-10 data-l1key="AbeilleActiver'.$i.'">'; ?>
                             <option value="N">{{Desactiver}}</option>
@@ -125,7 +125,7 @@
 
             <div id="Parametre">
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Tous les objets crées seront des enfants de l'objet selectionné. Les objets sont définis dans le menu principal de Jeedom: Outils->Objets. Les objets representent les pieces de la maison par exemple.">{{Objet Parent}}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Tous les objets crées seront des enfants de l'objet selectionné. Les objets sont définis dans le menu principal de Jeedom: Outils->Objets. Les objets representent les pieces de la maison par exemple.}}">{{Objet Parent}}</label>
                     <div class="col-lg-4">
                         <select class="configKey form-control" data-l1key="AbeilleParentId">
                             <?php
@@ -144,11 +144,11 @@
             <a class="btn btn-success" id="bt_zigatewifi_hide"><i class="fa fa-refresh"></i> {{Cache}}</a><a class="btn btn-danger" id="bt_zigatewifi_show"><i class="fa fa-refresh"></i> {{Affiche}}</a>
             <div id="zigatewifi">
                 <div>
-                    <p><i>La zigate wifi néccéssite l'installation de l'utilitaire socat pour lier un fichier fifo a une ip:port.</i></p>
+                    <p><i>{{La zigate wifi néccéssite l'installation de l'utilitaire socat pour lier un fichier fifo a une ip:port.}}</i></p>
                 </div>
 
                 <div class="form-group">
-                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="Installation de socat qui permet les échanges avec la zigate wifi.">{{Installation de socat}}</label>
+                    <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{Installation de socat qui permet les échanges avec la zigate wifi.}}">{{Installation de socat}}</label>
                     <div class="col-lg-5">
                         <a class="btn btn-warning" id="bt_installSocat"><i class="fa fa-refresh"></i> {{Installer}}</a>
                     </div>
@@ -164,9 +164,9 @@
         <a class="btn btn-success" id="bt_pizigate_hide"><i class="fa fa-refresh"></i> {{Cache}}</a><a class="btn btn-danger" id="bt_pizigate_show"><i class="fa fa-refresh"></i> {{Affiche}}</a>
         <div id="PiZigate">
             <div>
-                <p><i>La PiZiGate est controllée par un port TTY + 2x GPIO dépendant de votre plateforme.</i></p>
-                <p><i>Le logiciel 'WiringPi' (ou équivalent) est nécessaire pour piloter ces GPIOs.</i></p>
-                <p><i>Attention l'accès aux GPIOs ne se fait pas depuis un container sous Docker (si vous savez faire alors donnez moi la combine). Dans ce cas faites les manipulations à la main depuis le host.</i></p>
+                <p><i>{{La PiZiGate est controllée par un port TTY + 2x GPIO dépendant de votre plateforme.}}</i></p>
+                <p><i>{{Le logiciel 'WiringPi' (ou équivalent) est nécessaire pour piloter ces GPIOs.}}</i></p>
+                <p><i>{{Attention l'accès aux GPIOs ne se fait pas depuis un container sous Docker (si vous savez faire alors donnez moi la combine). Dans ce cas faites les manipulations à la main depuis le host.}}</i></p>
                 <!-- TODO: Possible known ports should be moved to "Connection" section.
                 <p><i>La PiZiGate est installée sur les pin GPIO du raspberry pi. Sur un RPI2, par defaut le port /dev/ttyAMA0 est disponible et utilisable. Sur un RPI3, il faut activer le port serie /dev/ttyS0.</i></p>
                 <p><i>Il faut aussi avoir le logiciel Wiring Pi installé pour faire un reset de la PiZIGate ou la programmer.</i></p>
@@ -175,20 +175,20 @@
             </div>
 
             <div class="form-group">
-                <label class="col-lg-4 control-label" data-toggle="tooltip" title="WiringPI est nécéssaire pour controller les GPIO.">{{Installation de WiringPi}}</label>
+                <label class="col-lg-4 control-label" data-toggle="tooltip" title="{{WiringPI est nécéssaire pour controller les GPIO.}}">{{Installation de WiringPi}}</label>
                 <div class="col-lg-5">
                     <a class="WiringPiStatus" title="Status du package">
                         <span class="label label-success" style="font-size:1em;">-?-</span>
                     </a>
-                    <a class="btn btn-warning" id="bt_checkWiringPi" title="Vérification de l'installation"><i class="fa fa-refresh"></i> {{Retester}}</a>
-                    <a class="btn btn-warning" id="bt_installWiringPi" title="Installation du package"><i class="fa fa-refresh"></i> {{Installer}}</a>
+                    <a class="btn btn-warning" id="bt_checkWiringPi" title="{{Vérification de l'installation}}"><i class="fa fa-refresh"></i> {{Retester}}</a>
+                    <a class="btn btn-warning" id="bt_installWiringPi" title="{{Installation du package}}"><i class="fa fa-refresh"></i> {{Installer}}</a>
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-lg-4 control-label">{{Port TTY}}</label>
                 <div class="col-lg-5">
-                    <select style="width:150px" id="ZiGatePort" data-toggle="tooltip" title="Port de communication avec la PiZigate">
+                    <select style="width:150px" id="ZiGatePort" data-toggle="tooltip" title="{{Port de communication avec la PiZigate}}">
                         <?php
                             /* Selecting default port.
                                TODO: Currently choosing first port but missing a way to confirm it is PiZigate */
@@ -205,39 +205,39 @@
                             }
                         ?>
                     </select>
-                    <a class="TTYStatus" title="Status de communication sur ce port">
+                    <a class="TTYStatus" title="{{Status de communication sur ce port}}">
                         <span class="label label-success" style="font-size:1em;">-?-</span>
                     </a>
-                    <a class="btn btn-warning" id="bt_checkTTY" title="Test de communication"><i class="fa fa-refresh"></i> {{Tester}}</a>
-                    <a class="btn btn-warning" id="bt_installTTY" title="Tentative d'activation du port"><i class="fa fa-refresh"></i> {{Activer}}</a>
+                    <a class="btn btn-warning" id="bt_checkTTY" title="{{Test de communication}}"><i class="fa fa-refresh"></i> {{Tester}}</a>
+                    <a class="btn btn-warning" id="bt_installTTY" title="{{Tentative d'activation du port}}"><i class="fa fa-refresh"></i> {{Activer}}</a>
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-lg-4 control-label">{{Firmware}}</label>
                 <div class="col-lg-5">
-                    <a class="CurrentFirmware" title="Version du firmware actuel">
+                    <a class="CurrentFirmware" title="{{Version du firmware actuel}}">
                         <span class="label label-success" style="font-size:1em;">-?-</span>
                     </a>
-                    <a class="btn btn-warning" id="bt_readFW" title="Lecture de la version du FW">
+                    <a class="btn btn-warning" id="bt_readFW" title="{{Lecture de la version du FW}}">
                         <i class="fa fa-refresh"></i>
                         {{Lire}}
                     </a>
-                    <select style="width:150px" id ="ZiGateFirmwareVersion" title="Firmwares disponibles">
+                    <select style="width:150px" id ="ZiGateFirmwareVersion" title="{{Firmwares disponibles}}">
                         <?php
                             foreach (ls('/var/www/html/plugins/Abeille/Zigate_Module/', '*.bin') as $value) {
                                 echo '<option value=' . $value . '>' . $value . '</option>';
                             }
                         ?>
                     </select>
-                    <a class="btn btn-warning" id="bt_updateFirmware" title="Programmation du FW selectionné"><i class="fa fa-refresh"></i> {{Mettre à jour}}</a>
+                    <a class="btn btn-warning" id="bt_updateFirmware" title="{{Programmation du FW selectionné}}"><i class="fa fa-refresh"></i> {{Mettre à jour}}</a>
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-lg-4 control-label">{{Reset (HW) PiZigate}}</label>
                 <div class="col-lg-5">
-                    <a class="btn btn-warning" id="bt_resetPiZigate" title="Reset HW de la PiZigate"><i class="fa fa-refresh"></i> {{Reset}}</a>
+                    <a class="btn btn-warning" id="bt_resetPiZigate" title="{{Reset HW de la PiZigate}}"><i class="fa fa-refresh"></i> {{Reset}}</a>
                 </div>
             </div>
         </div>

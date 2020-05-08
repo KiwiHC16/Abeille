e788003e5e5fb6a5073d9efbb1425dfd7eef264b
KiwiHC16 <moulinb@free.fr>
2020-05-08 11:29:32 +0200

    Limit to one Interrogate otherwise Zigate can't cope with it.

--- core/class/Abeille.class.php
+++ core/class/Abeille.class.php
@@ -107,6 +107,7 @@
                                 $cmd = "/usr/bin/nohup php /var/www/html/plugins/Abeille/core/class/AbeilleInterrogate.php ".$dest." ".$NE." >> /dev/null 2>&1 &";
                                 echo "Cmd: ".$cmd."\n";
                                 exec($cmd, $out, $status);
+                                return;
                             }
                         }
                     }
@@ -651,7 +652,7 @@
             exec(system::getCmdSudo()."kill -9 ".implode($output, ' ')." 2>&1");
 
             // Stop other deamon
-            exec("ps -e -o '%p %a' --cols=10000 | awk '/Abeille(Parser|SerialRead|Cmd|Socat).php /' | awk '{print $1}' | tr  '\n' ' '", $output);
+            exec("ps -e -o '%p %a' --cols=10000 | awk '/Abeille(Parser|SerialRead|Cmd|Socat|Interrogate).php /' | awk '{print $1}' | tr  '\n' ' '", $output);
             log::add('Abeille', 'debug', 'deamon stop: Killing deamons: '.implode($output, '!'));
             system::kill($output, true);
             exec(system::getCmdSudo()."kill -15 ".implode($output, ' ')." 2>&1");

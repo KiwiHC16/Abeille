08a04c4db05c0c04c0245d1dd984d9a17ed851a3
KiwiHC16 <moulinb@free.fr>
2020-06-04 12:31:11 +0200

    Remove Virtual Remote de la recuperation du niveau de bruit.

--- Network/refreshBruit.php
+++ Network/refreshBruit.php
@@ -16,11 +16,15 @@
     include_once("../resources/AbeilleDeamon/includes/fifo.php");
     include_once("../resources/AbeilleDeamon/includes/function.php");
         
+    // Je ne filtre pas sur les équipements qur batterie car j ai eu une réponse d'un interrupteur mural xiaomi. En les reveillant ca se trouve ils repondent.
     $eqLogics = Abeille::byType('Abeille');
     foreach ($eqLogics as $eqLogic) {
         if ( ($_GET['device'] == $eqLogic->getLogicalId()) || ($_GET['device'] == "All") ) {
-        Abeille::publishMosquitto( queueKeyAbeilleToCmd, priorityUserCmd, "Cmd".$eqLogic->getLogicalId()."/managementNetworkUpdateRequest", "" );
-        sleep(5);
+            list($type, $address, $action) = explode('/', $eqLogic->getLogicalId());
+            if ( strlen($address) == 4 ) {
+                Abeille::publishMosquitto( queueKeyAbeilleToCmd, priorityUserCmd, "Cmd".$eqLogic->getLogicalId()."/managementNetworkUpdateRequest", "" );
+                sleep(5);
+            }
         }
     }
 ?>

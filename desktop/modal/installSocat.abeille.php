9485f1eee855df7e3f08644488f3876a9678f00d
Tcharp38 <fabrice.charpentier@laposte.net>
2020-06-09 16:49:14 +0200

    Config: Zigate Wifi, socat check & installation

--- desktop/modal/installSocat.abeille.php
+++ desktop/modal/installSocat.abeille.php
@@ -1,22 +1,22 @@
 <?php
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
-}
+    /* This file is part of Jeedom.
+     *
+     * Jeedom is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * Jeedom is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+     */
+    if (!isConnect('admin')) {
+        throw new Exception('{{401 - Accès non autorisé}}');
+    }
 ?>
 <div id='div_installSocatAbeilleAlert' style="display: none;"></div>
 <a class="btn btn-warning pull-right" data-state="1" id="bt_abeilleLogStopStart"><i class="fa fa-pause"></i> {{Pause}}</a>
@@ -42,15 +42,15 @@ Lancement des operations.
         }
     });
 
-function updatelog(){
-    jeedom.log.autoupdate({
-                log: 'Abeille_installSocat',
-                display: $('#pre_abeilleInstallSocat'),
-                search: $('#in_abeilleLogSearch'),
-                control: $('#bt_abeilleLogStopStart'),
-            });
-}
+    function updatelog(){
+        jeedom.log.autoupdate({
+                    log: 'AbeilleConfig',
+                    display: $('#pre_abeilleInstallSocat'),
+                    search: $('#in_abeilleLogSearch'),
+                    control: $('#bt_abeilleLogStopStart'),
+                });
+    }
 
-setTimeout(updatelog,2000);
+    setTimeout(updatelog,2000);
 
 </script>

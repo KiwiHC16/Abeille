2a423324423e6cbc1c071a9a0671736002c48593
KiwiHC16 <moulinb@free.fr>
2020-08-26 10:39:11 +0200

    Ajouter dans la page support la version d'Abeille : fichier conf. #1230

--- desktop/modal/getCompleteAbeilleConf.php
+++ desktop/modal/getCompleteAbeilleConf.php
@@ -11,6 +11,29 @@
 
     global $CONFIG;
 
+    function getFileAndPrint( $file, $title, $printModal, $printFile ) {
+        
+        $contents = file_get_contents($file);
+        
+        if ( $printModal ) {
+            echo "<br>\n<br>\n";
+            echo "-----------------------<br>\n";
+            echo $title."<br>\n";
+            echo "-----------------------<br>\n";
+
+            echo $contents."<br><br>";
+        }
+        if ( $printFile ) {
+               log::add( 'AbeilleDbConf', 'info', '');
+               log::add( 'AbeilleDbConf', 'info', '-----------------------');
+               log::add( 'AbeilleDbConf', 'info', $title);
+               log::add( 'AbeilleDbConf', 'info', '-----------------------');
+                log::add( 'AbeilleDbConf', 'info', $contents);
+                log::add( 'AbeilleDbConf', 'info', '');
+           }
+        
+    }
+    
     function requestAndPrint( $link, $sql, $title, $printModal, $printFile) {
         if ( $printModal ) {
             echo "<br>\n<br>\n";
@@ -48,8 +71,13 @@
         if ( $printFile ) { log::add( 'AbeilleDbConf', 'info', '}'); }
     }
 
+    //------------------------------------------------------------------------------------------
+    // Main
+    //------------------------------------------------------------------------------------------
     echo "{{Extraction de toutes les informations necessaires à l'analyse du plugin.}}<br>\n";
 
+    getFileAndPrint('/var/www/html/plugins/Abeille/plugin_info/AbeilleVersion.inc', "{{Version from AbeilleVersion.inc}}", 1, 1);
+    
     $link = mysqli_connect( $CONFIG['db']['host'], $CONFIG['db']['username'], $CONFIG['db']['password'], $CONFIG['db']['dbname']);
 
     /* check connection */
@@ -58,7 +86,7 @@
         exit();
     }
 
-    requestAndPrint($link, "SELECT * FROM `update` WHERE `name` = 'Abeille'", "{{Version}}",                1, 1);
+    requestAndPrint($link, "SELECT * FROM `update` WHERE `name` = 'Abeille'", "{{Version from Jeedom DB}}", 1, 1);
     requestAndPrint($link, "select * from config where plugin = 'Abeille'", "{{Configuration du plugin}}",  1, 1);
     requestAndPrint($link, "SELECT * FROM `cron` WHERE `class` = 'Abeille'", "{{Liste des cron}}",          1, 1);
     requestAndPrint($link, "select * from eqLogic where eqType_name = 'Abeille'", "{{Liste des abeilles}}", 1, 1);

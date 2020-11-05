6d415227bdc0839fe6b2c897e26cbd4acf171419
Fabrice Charpentier <fabrice.charpentier@st.com>
2020-11-05 14:50:11 +0100

    Health page: Added support of disabled status

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -46,18 +46,18 @@ Démons:
             echo "\n\n\n\n<tr>".'<td><a href="'.$eqLogic->getLinkToConfiguration().'" style="text-decoration: none;">'.$eqLogic->getHumanName(true).'</a></td>';
 
             // Module type
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">'.$eqLogic->getConfiguration('icone').'</span></td>';
+            echo '<td><span class="label label-info" style="font-size: 1em; cursor: default;">'.$eqLogic->getConfiguration('icone').'</span></td>';
 
             // ID
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">'.$eqLogic->getId().'</span></td>';
+            echo '<td><span class="label label-info" style="font-size: 1em; cursor: default;">'.$eqLogic->getId().'</span></td>';
 
             $parts = explode("/", $eqLogic->getLogicalId());
 
             // Ruche
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">'.$parts[0].'</span></td>';
+            echo '<td><span class="label label-info" style="font-size: 1em; cursor: default;">'.$parts[0].'</span></td>';
 
             // Short Address
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">'.$parts[1].'</span></td>';
+            echo '<td><span class="label label-info" style="font-size: 1em; cursor: default;">'.$parts[1].'</span></td>';
 
             /* Extended address/IEEE
                If present in config, taking it.
@@ -83,48 +83,47 @@ Démons:
                 }
             }
             if ((strlen($addrIEEE) == 16) || ($addrIEEE=="-")) {
-                echo '<td><span class="label label-success" style="font-size : 1em; cursor : default;">'.$addrIEEE.'</span></td>';
+                echo '<td><span class="label label-success" style="font-size: 1em; cursor: default;">'.$addrIEEE.'</span></td>';
             } else {
-                echo '<td><span class="label label-warning" style="font-size : 1em; cursor : default;">Manquante</span></td>';
+                echo '<td><span class="label label-warning" style="font-size: 1em; cursor: default;">Manquante</span></td>';
             }
 
             // Status
             // Status Ok par defaut, apres on test et on met le status à la valeur voulue
-            $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">{{OK}}</span>';
-            if ( (time() - strtotime($eqLogic->getStatus('lastCommunication'))) > (60*$eqLogic->getTimeout()) ) {
-                 $status = '<span class="label label-warning" style="font-size : 1em; cursor : default;">Time Out Last Communication</span>';
-            }
-            if ( (time() - strtotime($eqLogic->getStatus('lastCommunication'))) > ((2*60*$eqLogic->getTimeout())) ) {
-                $status = '<span class="label label-danger" style="font-size : 1em; cursor : default;">Time Out Last Communication</span>';
-            }
-            if ($eqLogic->getStatus('state') == '-') {
-                $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">-</span>';
-            }
-            if ( $eqLogic->getConfiguration('icone') == "remotecontrol" ) {
-                $status = '<span class="label label-success" style="font-size : 1em; cursor : default;">-</span>';
-            }
+            if ($eqLogic->getIsEnable() == 0) // Disabled ?
+                $status = '<span class="label label-default" style="font-size: 1em; cursor: default;">{{Dévalidé}}</span>';
+            else if ($eqLogic->getStatus('state') == '-')
+                $status = '<span class="label label-success" style="font-size: 1em; cursor: default;">-</span>';
+            else if ($eqLogic->getConfiguration('icone') == "remotecontrol")
+                $status = '<span class="label label-success" style="font-size: 1em; cursor: default;">-</span>';
+            else if ((time() - strtotime($eqLogic->getStatus('lastCommunication'))) > ((2*60*$eqLogic->getTimeout())))
+                $status = '<span class="label label-danger" style="font-size: 1em; cursor: default;">Time-out</span>';
+            else if ((time() - strtotime($eqLogic->getStatus('lastCommunication'))) > (60*$eqLogic->getTimeout()))
+                $status = '<span class="label label-warning" style="font-size: 1em; cursor: default;">Time-out</span>';
+            else
+                $status = '<span class="label label-success" style="font-size: 1em; cursor: default;">{{OK}}</span>';
             echo '<td>'.$status.'</td>';
 
             // Derniere Comm
-            $lastComm = '<span class="label label-warning" style="font-size : 1em; cursor : default;">No message received !!</span>';
+            $lastComm = '<span class="label label-warning" style="font-size: 1em; cursor: default;">No message received !!</span>';
             if ( strlen($eqLogic->getStatus('lastCommunication'))>2 ) {
-                $lastComm = '<span class="label label-info" style="font-size : 1em; cursor : default;">'.$eqLogic->getStatus('lastCommunication').'</span>';
+                $lastComm = '<span class="label label-info" style="font-size: 1em; cursor: default;">'.$eqLogic->getStatus('lastCommunication').'</span>';
             }
             if ( $eqLogic->getConfiguration('icone') == "remotecontrol" ) {
-                $lastComm = '<span class="label label-info" style="font-size : 1em; cursor : default;">-</span>';
+                $lastComm = '<span class="label label-info" style="font-size: 1em; cursor: default;">-</span>';
             }
             echo '<td>'.$lastComm.'</td>';
 
             // Depuis
-            $Depuis = '<span class="label label-info" style="font-size : 1em; cursor : default;">'.(floor((time() - strtotime($eqLogic->getStatus('lastCommunication'))) / 3600)).'</span>';
+            $Depuis = '<span class="label label-info" style="font-size: 1em; cursor: default;">'.(floor((time() - strtotime($eqLogic->getStatus('lastCommunication'))) / 3600)).'</span>';
              if ( $eqLogic->getConfiguration('icone') == "remotecontrol" ) {
-                 $Depuis = '<span class="label label-info" style="font-size : 1em; cursor : default;">-</span>';
+                 $Depuis = '<span class="label label-info" style="font-size: 1em; cursor: default;">-</span>';
              }
-            //if ($eqLogic->getStatus('state') == '-') { $Depuis = '<span class="label label-info" style="font-size : 1em; cursor : default;">-</span>'; }
+            //if ($eqLogic->getStatus('state') == '-') { $Depuis = '<span class="label label-info" style="font-size: 1em; cursor: default;">-</span>'; }
             echo '<td>'.$Depuis.'</td>';
 
             // Date Creation
-            echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">'.$eqLogic->getConfiguration('createtime').'</span></td></tr>';
+            echo '<td><span class="label label-info" style="font-size: 1em; cursor: default;">'.$eqLogic->getConfiguration('createtime').'</span></td></tr>';
         }
     ?>
     </tbody>

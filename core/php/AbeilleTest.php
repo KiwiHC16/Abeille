df53b2cf5ba701fb9d778d0657f14150a73f6f7d
KiwiHC16 <moulinb@free.fr>
2020-11-19 18:15:53 +0100

    Envoie de commande en direct.

---
+++ core/php/AbeilleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+    include_once __DIR__.'/../../../../core/php/core.inc.php';
+    include_once __DIR__.'/../../resources/AbeilleDeamon/includes/config.php';
+    include_once __DIR__.'/../../resources/AbeilleDeamon/includes/function.php';
+    include_once __DIR__.'/../../resources/AbeilleDeamon/includes/fifo.php';
+
+    include_once __DIR__.'/AbeilleLog.php';
+
+    include_once __DIR__.'/../class/AbeilleCmdQueue.class.php';
+
+    $objet = new AbeilleCmdQueue('debug');
+    $message = new object();
+    $message->priority = 1;
+
+    if (0) {
+        $objet->sendCmdToZigate( 'Abeille1', '0092', '0006', "0283E4010102" );
+    }
+
+    if (1) {
+        if (1) {
+            $message->topic = 'CmdAbeille1/2317/moveToLiftAndTiltBSO';
+            $message->payload = 'EP=01&Inclinaison=60&duration=FFFF';
+        }  
+
+        
+        if (0) {
+            $message->topic = 'CmdAbeille1/83E4/OnOff';
+            $message->payload = 'Action=Toggle&EP=01';
+        }
+
+        if (0) {
+            $message->topic = 'CmdAbeille1/83E4/OnOff';
+            $message->payload = 'Action=Toggle&EP=01';
+        }
+
+        $objet->procmsg($message);
+        $objet->processCmdQueueToZigate();
+    }
+?>

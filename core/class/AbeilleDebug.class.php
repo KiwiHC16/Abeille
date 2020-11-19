5d610ded767d8a2d3012375bf2cfb93de886f174
KiwiHC16 <moulinb@free.fr>
2020-11-19 15:33:49 +0100

    Extracted classes from AbeilleCmd

---
+++ core/class/AbeilleDebug.class.php
@@ -0,0 +1,36 @@
+<?php
+class debug extends AbeilleTools {
+        public $debug = array(
+            "cli"                    => 0, // commande line mode or jeedom
+            "Checksum"                => 0, // Debug checksum calculation
+            "tempo"                   => 0, // Debug tempo queue
+            "procmsg"                 => 0, // Debug fct procmsg
+            "procmsg1"                => 1, // Debug fct procmsg avec un seul msg
+            "procmsg2"                => 1, // Debug fct procmsg avec un seul msg
+            "procmsg3"                => 0, // Debug fct procmsg avec un seul msg
+            "processCmd"              => 1, // Debug fct processCmd
+            "sendCmd"                 => 1, // Debug fct sendCmd
+            "sendCmd2"                => 0, // Debug fct sendCmd
+            "cmdQueue"                => 0, // Debug cmdQueue
+            "sendCmdAck"              => 1, // Debug fct sendCmdAck
+            "sendCmdAck2"             => 0, // Debug fct sendCmdAck
+            "transcode"               => 0, // Debug transcode fct
+            "AbeilleCmdClass"         => 1, // Mise en place des class
+            "sendCmdToZigate"         => 1, // Mise en place des class
+            "traiteLesAckRecus"       => 0, // Nouvelle Gestion des Ack
+            "processCmdQueueToZigate" => 1,
+        );
+
+        function deamonlog($loglevel = 'NONE', $message = "", $isEnable = 1)
+        {
+            if ($isEnable == 0) {
+                return;
+            }
+            if ($this->debug["cli"]) {
+                echo "[".date("Y-m-d H:i:s").'][AbeilleCmd][DEBUG.KIWI] '.$message."\n";
+            } else {
+                logMessage($loglevel, $message);
+            }
+        }
+    }
+?>

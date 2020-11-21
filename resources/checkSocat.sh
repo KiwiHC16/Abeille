50fdc79e5f1d9218c309b391defbb3ef8bf56081
edgd1er <edgd1er@hotmail.com>
2020-11-21 07:20:02 +0100

    test gpio, ajout d'info dans la configuration pour docker
    présence et executabilité de gpio
    info options pour rendre gpio disponible dans le conteneur

--- resources/checkSocat.sh
+++ resources/checkSocat.sh
@@ -5,7 +5,7 @@ echo "[${NOW}] Démarrage de '$(basename $0)'"
 
 # 'socat'
 echo "Vérification de l'installation du package 'socat'"
-hash socat 2>/dev/null
+command -v socat
 if [ $? -ne 0 ]; then
     echo "= ERREUR: Commande 'socat' manquante !"
     echo "=         Le package 'socat' semble mal installé."

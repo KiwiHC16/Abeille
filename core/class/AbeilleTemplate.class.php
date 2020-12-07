d0fa1af2e836ce5bfaeeb63a155571cfcf0079f5
KiwiHC16 <moulinb@free.fr>
2020-12-07 18:50:21 +0100

    getNameJeedomFromTemplate

--- core/class/AbeilleTemplate.class.php
+++ core/class/AbeilleTemplate.class.php
@@ -59,28 +59,42 @@ class AbeilleTemplate
     }
 
     /**
-     * Will return the json file for a template uniqId
+     * Will return the json array for a template uniqId
      *
-     * @return          Return the json file for a template uniqId or -1 if not found
+     * @return          Return the json array for a template uniqId or -1 if not found
      */
     public static function getJsonForUniqId( $uniqId ) {
         $jsonText = file_get_contents( self::getJsonFileNameForUniqId( $uniqId ));
-        $json = json_decode($jsonText);
-        return $json;
+        $jsonArray = json_decode($jsonText, true);
+        return $jsonArray;
     }
+
+    /**
+     * Will return the name o fthe device store n the template
+     *
+     * @return          Return the name o fthe device store n the template
+     */
+    public static function getNameJeedomFromTemplate( $uniqId ) {
+        $jsonArray = self::getJsonForUniqId( $uniqId );
+        foreach ( $jsonArray as $key=>$data ) {
+            return $jsonArray[$key]["nameJeedom"];
+        }
+    }
+
 }
 
 // '5c07c76620sdsfs8a7'
 // Abeille1/3EFE
 
-$test = new AbeilleTemplate();
+$test = new AbeilleTemplate;
 
 // var_dump( $test->uniqIdUsedByAnAbeille('Abeille1/3EFE') );
 // var_dump( $test->getEqLogicsByTemplateUniqId('5c07c76620sdsfs8a7') );
-
 // var_dump( $test->getJsonFileNameForUniqId('5c07c76620sdsfs8a7') );
+// var_dump( $test->getJsonForUniqId( '5c07c76620sdsfs8a7' ) );
 
-var_dump( $test->getJsonForUniqId( '5c07c76620sdsfs8a7' ) );
+var_dump( $test->getNameJeedomFromTemplate('5c07c76620sdsfs8a7') );
+
 
 
 ?>

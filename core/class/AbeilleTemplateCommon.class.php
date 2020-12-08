73de0a263d0164174715c8fc925a5a67cb007b04
KiwiHC16 <moulinb@free.fr>
2020-12-08 15:25:51 +0100

    Correct 2 bugs.

--- core/class/AbeilleTemplateCommon.class.php
+++ core/class/AbeilleTemplateCommon.class.php
@@ -12,7 +12,7 @@ class AbeilleTemplateCommon
         $templateFiles = glob('/var/www/html/plugins/Abeille/core/config/devices/*/*.json'); 
         foreach( $templateFiles as $templateFile ) {
             $json = file_get_contents( $templateFile );
-            if ( strpos($json, $uniqId) > 1 ) {
+            if ( strpos($json, '"'.$uniqId.'"') > 1 ) {
                 return $templateFile;
             }
         }
@@ -25,7 +25,9 @@ class AbeilleTemplateCommon
      * @return          Return the json array for a template uniqId or -1 if not found
      */
     public static function getJsonForUniqId( $uniqId ) {
-        $jsonText = file_get_contents( self::getJsonFileNameForUniqId( $uniqId ));
+        $file = self::getJsonFileNameForUniqId( $uniqId );
+        if ( $file == -1 ) return -1;
+        $jsonText = file_get_contents( $file );
         $jsonArray = json_decode($jsonText, true);
         return $jsonArray;
     }

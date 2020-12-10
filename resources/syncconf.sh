b00e10258d448fbb333715f144af08dd2a44388e
KiwiHC16 <moulinb@free.fr>
2020-12-10 10:01:57 +0100

    Comments in relation to Mise a jour des equipements sur la base des nouveaux modeles #1511

--- resources/syncconf.sh
+++ resources/syncconf.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# This script will download all templates with their images from GitHub master to install it user system.
+# Should be used in conjonction with AbeilleTemplate*.class.php
+# 20201210: Probably not visible to end user at this stage as dev on going.
+# Should not be removed.
+# Idea is to allow user to download latest template and test on his system.
+
 echo "Lancement du téléchargement des configurations"
 
 cd /var/www/html/plugins/Abeille/core/config/
@@ -12,6 +19,11 @@ cd tmp
 
 echo "Récupération des sources (cette étape peut durer quelques minutes)"
 wget https://github.com/KiwiHC16/Abeille/archive/master.zip
+
+# Should try :
+# unzip /path/to/archive.zip 'in/archive/folder/*' -d /path/to/unzip/to
+# to simply all and reduce size used on disk.
+
 unzip master.zip
 
 cd /var/www/html/plugins/Abeille/tmp/Abeille-master

b765701cccafc56735c2896ccafba51fe9b2585c
Fabrice Charpentier <fabrice.charpentier@st.com>
2020-12-18 17:12:44 +0100

    Cosmetic

--- Network/TestSVG/TopoGet.php
+++ Network/TestSVG/TopoGet.php
@@ -1,8 +1,8 @@
 <?php
     require_once __DIR__."/../../../../core/php/core.inc.php";
-    
+
     $table = array();
-    
+
     $eqLogics = eqLogic::byType('Abeille');
     foreach ($eqLogics as $eqLogic) {
         $eqParent = $eqLogic->getObject();
@@ -16,10 +16,10 @@
         $device['id']           = $eqLogic->getId();
         $device['X']            = max( 0, $eqLogic->getConfiguration('positionX'));
         $device['Y']            = max( 0, $eqLogic->getConfiguration('positionY'));
-        
+
         $table[$eqLogic->getLogicalId()] = $device;
         unset( $device );
     }
-    
+
     echo json_encode($table);
-    ?>
+?>

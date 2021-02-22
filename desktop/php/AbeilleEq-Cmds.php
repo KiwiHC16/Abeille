c2719573ffa366350730eacd5d6751f9e96ff5ba
Fabrice Charpentier <fabrice.charpentier@st.com>
2021-02-22 17:29:01 +0100

    Eq page, commands: New command hidden when not in dev mode

--- desktop/php/AbeilleEq-Cmds.php
+++ desktop/php/AbeilleEq-Cmds.php
@@ -3,22 +3,19 @@
 
 <form class="form-horizontal">
     <fieldset>
-        <!-- <div class="form-actions">
-            <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i>  {{Ajouter une commande}}</a>
-        </div> -->
-        <div class="form-actions">
-            <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleAction"><i class="fa fa-plus-circle"></i>  {{Ajouter une commande action}}</a>
-            <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleInfo">  <i class="fa fa-plus-circle"></i>  {{Ajouter une commande info}}</a>
-        </div>
+        <?php if (isset($dbgDeveloperMode) && ($dbgDeveloperMode == TRUE)) { ?>
+            <div class="form-actions">
+                <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleAction"><i class="fa fa-plus-circle"></i>  {{Ajouter une commande action}}</a>
+                <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleInfo">  <i class="fa fa-plus-circle"></i>  {{Ajouter une commande info}}</a>
+            </div>
+        <?php }?>
     </fieldset>
 </form>
 <br/>
 <table id="table_cmd" class="table table-bordered table-condensed">
     <thead>
     <tr>
-        <?php
-            if (isset($dbgDeveloperMode) && ($dbgDeveloperMode == TRUE)) {
-        ?>
+        <?php if (isset($dbgDeveloperMode) && ($dbgDeveloperMode == TRUE)) { ?>
                 <th style="width:  80px;">{{#}}</th>
                 <th style="width: 150px;">{{Commande Jeedom}}</th>
                 <th style="width: 120px;">{{Sous-Type}}</th>
@@ -28,10 +25,7 @@
                 <th style="width: 130px;">{{Options}}</th>
                 <th style="width:  20px;"></th>
                 <th style="width: 80px;">{{Supprimer}}</th>
-        <?php
-            }
-            else {
-        ?>
+        <?php } else { ?>
                 <th style="width:  80px;">{{#}}</th>
                 <th style="width: 150px;">{{Commande Jeedom}}</th>
                 <th style="width: 120px;">{{Sous-Type}}</th>
@@ -39,9 +33,7 @@
                 <th style="width: 130px;">{{Options}}</th>
                 <th style="width:  20px;"></th>
                 <th style="width: 80px;">{{Supprimer}}</th>
-        <?php
-             }
-        ?>
+        <?php }?>
 
     </tr>
     </thead>

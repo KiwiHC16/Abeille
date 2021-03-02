0d724da10717c8eef855f97d4cec1d8926c14492
Fabrice Charpentier <fabrice.charpentier@st.com>
2021-03-02 13:32:31 +0100

    Update for #1829

--- desktop/php/AbeilleEq-Cmds.php
+++ desktop/php/AbeilleEq-Cmds.php
@@ -8,36 +8,26 @@
                 <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleAction"><i class="fa fa-plus-circle"></i>  {{Ajouter une commande action}}</a>
                 <a class="btn btn-success btn-sm pull-right" id="bt_addAbeilleInfo">  <i class="fa fa-plus-circle"></i>  {{Ajouter une commande info}}</a>
             </div>
-        <?php }?>
+        <?php } ?>
     </fieldset>
 </form>
 <br/>
 <table id="table_cmd" class="table table-bordered table-condensed">
     <thead>
-    <tr>
-        <?php if (isset($dbgDeveloperMode) && ($dbgDeveloperMode == TRUE)) { ?>
-                <th style="width:  80px;">{{#}}</th>
-                <th style="width: 150px;">{{Commande Jeedom}}</th>
-                <th style="width: 120px;">{{Sous-Type}}</th>
+        <tr>
+            <th style="width:  80px;">{{#}}</th>
+            <th style="width: 150px;">{{Commande Jeedom}}</th>
+            <th style="width: 120px;">{{Type}}</th>
+            <?php if (isset($dbgDeveloperMode) && ($dbgDeveloperMode == TRUE)) { ?>
                 <th style="width: 300px;">{{Commande Abeille}}</th>
                 <th style="width: 600px;">{{Paramètres commande Abeille}}</th>
-                <th style="width: 150px;">{{Unité/Cron}}</th>
-                <th style="width: 130px;">{{Options}}</th>
-                <th style="width:  20px;"></th>
-                <th style="width: 80px;">{{Supprimer}}</th>
-        <?php } else { ?>
-                <th style="width:  80px;">{{#}}</th>
-                <th style="width: 150px;">{{Commande Jeedom}}</th>
-                <th style="width: 120px;">{{Sous-Type}}</th>
-                <th style="width: 150px;">{{Unité/Cron}}</th>
-                <th style="width: 130px;">{{Options}}</th>
-                <th style="width:  20px;"></th>
-                <th style="width: 80px;">{{Supprimer}}</th>
-        <?php }?>
-
-    </tr>
+            <?php } ?>
+            <th style="width: 150px;">{{Unité/Cron}}</th>
+            <th style="width: 130px;">{{Options}}</th>
+            <th style="width:  20px;"></th>
+            <th style="width: 80px;">{{Supprimer}}</th>
+        </tr>
     </thead>
     <tbody>
-
     </tbody>
 </table>

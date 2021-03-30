7be02dce8cc4ddae44432ec65e2104ffd359bf93
Fabrice Charpentier <fabrice.charpentier@st.com>
2021-03-30 11:43:55 +0200

    AbeilleCmd: Added reroute support for EQ discovery assistant

--- resources/AbeilleDeamon/AbeilleCmd.php
+++ resources/AbeilleDeamon/AbeilleCmd.php
@@ -1,6 +1,6 @@
 <?php
     /*
-     * démon AbeilleCmd
+     * AbeilleCmd daemon
      *
      * subscribe to Abeille topic and receive message sent by AbeilleParser.
      *
@@ -9,10 +9,13 @@
     /* Developers debug features */
     $dbgFile = __DIR__."/../../tmp/debug.json";
     if (file_exists($dbgFile)) {
-        // include_once $dbgFile;
+        $dbgConfig = json_decode(file_get_contents($dbgFile), TRUE);
+        if (isset($dbgConfig["dbgMonitorAddr"])) { // Monitor params
+            $dbgMonitorAddr = $dbgConfig["dbgMonitorAddr"];
+        }
         /* Dev mode: enabling PHP errors logging */
         error_reporting(E_ALL);
-        ini_set('error_log', __DIR__.'/../../../../log/AbeillePHP.log');
+        ini_set('error_log', __DIR__.'/../../../log/AbeillePHP.log');
         ini_set('log_errors', 'On');
     }
 
@@ -35,10 +38,9 @@
     $parameters = AbeilleTools::getParameters();
     $running = AbeilleTools::getRunningDaemons();
     $daemons= AbeilleTools::diffExpectedRunningDaemons($parameters,$running);
-    logMessage('info', 'status des daemons: '.json_encode($daemons));
-    #Two at least expected,the original and this one
+    logMessage('debug', 'Daemons status: '.json_encode($daemons));
     if ($daemons["cmd"] > 1){
-        logMessage('error', 'Le daemon est déja lancé! '.json_encode($daemons));
+        logMessage('error', 'Le démon est déja lancé ! '.json_encode($daemons));
         exit(3);
     }
 
@@ -46,6 +48,8 @@
         $AbeilleCmdQueue = new AbeilleCmdQueue($argv[1]);
 
         $last = 0;
+        $fromAssistQueue = msg_get_queue(queueKeyAssistToCmd);
+        $rerouteNet = ""; // Rerouted network if defined (ex: 'Abeille1')
         while (true) {
             /* Treat Zigate statuses (0x8000 cmd) coming from parser */
             $AbeilleCmdQueue->traiteLesAckRecus();
@@ -54,6 +58,36 @@
 
             // Traite toutes les commandes zigate en attente
             $AbeilleCmdQueue->processCmdQueueToZigate();
+
+            /* Performing msg rerouting from 'EQ assistant' */
+            $max_msg_size = 2048;
+            $msg_type = NULL;
+            if (msg_receive($fromAssistQueue, 0, $msg_type, $max_msg_size, $msg, TRUE, MSG_IPC_NOWAIT) == TRUE) {
+                logMessage('debug', "Received=".json_encode($msg));
+                if ($msg['type'] == 'reroute') {
+                    $rerouteNet = $msg['network'];
+                    /* TODO: Tcharp38: Can be optimized */
+                    $zgNb = str_replace('Abeille', '', $rerouteNet);
+                    $AbeilleCmdQueue->zigateAvailable[$zgNb] = 0;
+                    logMessage('debug', "'".$rerouteNet."' messages must be rerouted");
+                } else if ($msg['type'] == 'reroutestop') {
+                    logMessage('debug', "Stopping '".$rerouteNet."' msg rerouting.");
+                    $rerouteNet = "";
+                    /* TODO: Tcharp38: Can be optimized */
+                    $zgNb = str_replace('Abeille', '', $rerouteNet);
+                    $AbeilleCmdQueue->zigateAvailable[$zgNb] = 1;
+                } else if ($msg['type'] == 'msg') {
+                    logMessage('debug', $rerouteNet.", rerouting: ".$msg['msg']);
+                    /* TODO: Tcharp38: Can be optimized */
+                    $zgNb = str_replace('Abeille', '', $rerouteNet);
+                    if (config::byKey('AbeilleActiver'.$zgNb, 'Abeille', 'N') == 'Y' ) {
+                        $sp = config::byKey('AbeilleSerialPort'.$zgNb, 'Abeille', '1', 1);
+                        $f = fopen($sp, "w");
+                        fwrite($f, pack("H*", $msg['msg']));
+                        fclose($f);
+                    }
+                }
+            }
 
             $AbeilleCmdQueue->recupereTousLesMessagesVenantDesAutresThreads();
 

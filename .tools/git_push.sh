be76607e9c3e434ceab486b71fd801f7c1e47b81
Tcharp38 <fabrice.charpentier@laposte.net>
2021-04-21 11:27:27 +0200

    Script: git_push update

--- .tools/git_push.sh
+++ .tools/git_push.sh
@@ -32,31 +32,19 @@ REMOTE_BRANCH=${R[1]} # Ex: 'master'
 echo "  REMOTE_SOURCE=${REMOTE_REPO}"
 echo "  REMOTE_BRANCH=${REMOTE_BRANCH}"
 
-# Updating Abeille's version
-# Selected format is compact but complete.
-# Format: YYMMDD-REMOTE_BRANCH-X (ex: 210204-STABLE-1)
-echo "Computing new version"
-DATE_NEW=`date  +"%y%m%d"`
-REM_BRANCH_UP=`echo "${REMOTE_BRANCH}" | tr '[:lower:]' '[:upper:]'`
-VERSION_PREFIX="${DATE_NEW}-${REM_BRANCH_UP}-"
-echo "  VERSION_PREFIX=${VERSION_PREFIX}"
-VERSION_CURRENT=`cat plugin_info/Abeille.version | tail -1`
-echo "- Current version=${VERSION_CURRENT}"
-MINOR_CURRENT=0
-if [[ "${VERSION_CURRENT}" == "${VERSION_PREFIX}"* ]]; then
-    if [ "${VERSION_CURRENT}" != "" ]; then
-        MINOR_CURRENT=${VERSION_CURRENT#${VERSION_PREFIX}}
-    fi
+# Updating plugin version
+.tools/update_version.sh ${REMOTE_BRANCH}
+if [ $? -ne 0 ]; then
+    echo "= ERROR"
+    exit 22
 fi
-echo "  MINOR_CURRENT=${MINOR_CURRENT}"
-MINOR_NEW=`expr ${MINOR_CURRENT} + 1`
-VERSION_NEW="${VERSION_PREFIX}${MINOR_NEW}"
-echo "- New version = ${VERSION_NEW}"
-echo "# Auto-generated Abeille's version" >plugin_info/Abeille.version
-echo "${VERSION_NEW}" >>plugin_info/Abeille.version
 
-# Commit ready. Generating md5 for versionned files
-.tools/gen_md5.sh
+# Updating MD5 file
+.tools/update_md5.sh
+if [ $? -ne 0 ]; then
+    echo "= ERROR"
+    exit 23
+fi
 
 # Add + commit + push
 echo "Updating GIT & pushing"

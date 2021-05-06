82714ca95b6fe5f24bc28b07319eba3e845378af
Tcharp38 <fabrice.charpentier@laposte.net>
2021-05-06 18:58:29 +0200

    check_json enhancements

--- .tools/check_json.php
+++ .tools/check_json.php
@@ -2,18 +2,35 @@
     /* Check JSON files */
 
     define('devicesDir', __DIR__.'/../core/config/devices');
+    define('commandsDir', __DIR__.'/../core/config/commands');
+
     $devicesList = [];
+    $commandsList = [];
+    $missingCmds = 0;
 
-    function checkDevice() {
+    function checkDevice($devName, $dev) {
+        global $missingCmds;
 
+        if (!isset($dev[$devName])) {
+            echo "  ERROR: Corruped JSON. Missing ".$devName." key\n";
+            return;
+        }
+        // echo "dev=".json_encode($dev)."\n";
+        $cmds = $dev[$devName]['Commandes'];
+        // echo "cmds=".json_encode($cmds)."\n";
+        foreach ($cmds as $key => $value) {
+            $path = commandsDir."/".$value.".json";
+            if (!file_exists($path)) {
+                echo "  ERROR: Unknown command ".$value."\n";
+                $missingCmds++;
+            }
+        }
     }
 
     function checkCommand() {
 
     }
 
-    /* If JSON name not given on cmd line, parsing all
-       devices & commands */
     function buildDevicesList() {
         echo "Building devices list ...\n";
         global $devicesList;
@@ -31,26 +48,53 @@
                 continue;
             }
 
-            $devicesList[] = $fullPath;
+            $devicesList[$dirEntry] = $fullPath;
         }
     }
 
+    function buildCommandsList() {
+        echo "Building commands list ...\n";
+        global $commandsList;
+        $commandsList = [];
+        $dh = opendir(commandsDir);
+        while (($dirEntry = readdir($dh)) !== false) {
+             /* Ignoring some entries */
+             if (in_array($dirEntry, array(".", "..")))
+                continue;
+
+            $fullPath = commandsDir.'/'.$dirEntry;
+            if (!file_exists($fullPath)) {
+                echo "- ".$dirEntry.": path access ERROR\n";
+                echo "  ".$fullPath."\n";
+                continue;
+            }
+
+            $commandsList[$dirEntry] = $fullPath;
+        }
+    }
+
+    /* If JSON name not given on cmd line, parsing all
+       devices & commands */
     buildDevicesList();
+    buildCommandsList();
 
     // echo "devl=".json_encode($devicesList)."\n";
     echo "Checking devices\n";
     $errors = 0;
-    foreach ($devicesList as $fullPath) {
-        echo "- ".$fullPath."\n";
+    foreach ($devicesList as $entry => $fullPath) {
+        echo "- ".$entry."\n";
         $jsonContent = file_get_contents($fullPath);
         $content = json_decode($jsonContent, true);
         if (json_last_error() != JSON_ERROR_NONE) {
-            echo "= Corrupted JSON file\n";
+            echo "  ERROR: Corrupted JSON file\n";
             $errors++;
         }
+        checkDevice($entry, $content);
     }
     if ($errors != 0)
         echo "= WARNING: Some corrupted devices found\n";
-    else
+    if ($missingCmds != 0)
+        echo "= WARNING: Some missing commands found\n";
+    if (($errors == 0) && ($missingCmds == 0))
         echo "= Devices ok\n";
 ?>

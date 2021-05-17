7eed3486c06e31170d9b42989fb849f482ad2bb2
Tcharp38 <fabrice.charpentier@laposte.net>
2021-05-18 00:16:19 +0200

    check_json: script enhanced

--- .tools/check_json.php
+++ .tools/check_json.php
@@ -7,28 +7,77 @@
     $devicesList = [];
     $commandsList = [];
     $missingCmds = 0;
+    $devErrors = []; // Errors/warnings found in devices
+    $cmdErrors = []; // Errors/warnings found in commands
+
+    /* Register a new device error/warning */
+    function newDevError($file, $type, $msg) {
+        echo "  ".$type.": ".$msg."\n";
+
+        global $devErrors;
+        $e = array(
+            "file" => $file,
+            "type" => $type,
+            "msg" => $msg
+        );
+        $devErrors[] = $e;
+    }
+
+    /* Register a new command error/warning */
+    function newCmdError($file, $type, $msg) {
+        echo "  ".$type.": ".$msg."\n";
+
+        global $cmdErrors;
+        $e = array(
+            "file" => $file,
+            "type" => $type,
+            "msg" => $msg
+        );
+        $cmdErrors[] = $e;
+    }
 
     function checkDevice($devName, $dev) {
         global $missingCmds;
+        global $commandsList;
 
         if (!isset($dev[$devName])) {
-            echo "  ERROR: Corruped JSON. Missing ".$devName." key\n";
+            newDevError($devName, "ERROR", "Corruped JSON. Expecting '".$devName."' top key");
             return;
         }
         // echo "dev=".json_encode($dev)."\n";
+        if (!isset($dev[$devName]['Commandes'])) {
+            newDevError($devName, "WARNING", "No commands defined");
+            return;
+        }
         $cmds = $dev[$devName]['Commandes'];
         // echo "cmds=".json_encode($cmds)."\n";
         foreach ($cmds as $key => $value) {
             $path = commandsDir."/".$value.".json";
             if (!file_exists($path)) {
-                echo "  ERROR: Unknown command ".$value."\n";
+                newDevError($devName, "ERROR", "Unknown command ".$value);
                 $missingCmds++;
+            } else {
+                $commandsList[$value] = $path;
             }
         }
     }
 
-    function checkCommand() {
+    function checkCommand($cmdName, $cmd) {
+        global $commandsList;
 
+        if (!isset($cmd[$cmdName])) {
+            newCmdError($cmdName, "ERROR", "Expecting '".$cmdName."' top key");
+            return;
+        }
+
+        $c = $cmd[$cmdName];
+        if (!isset($c['configuration'])) {
+            newCmdError($cmdName, "ERROR", "Missing 'configuration' section");
+        } else {
+            if (!isset($c['configuration']['topic'])) {
+                newCmdError($cmdName, "ERROR", "Missing 'configuration:topic' field");
+            }
+        }
     }
 
     function buildDevicesList() {
@@ -38,7 +87,7 @@
         $dh = opendir(devicesDir);
         while (($dirEntry = readdir($dh)) !== false) {
              /* Ignoring some entries */
-             if (in_array($dirEntry, array(".", "..", "listeCompatibilite.php")))
+             if (in_array($dirEntry, array(".", "..", "LISEZMOI.txt", "README.txt")))
                 continue;
 
             $fullPath = devicesDir.'/'.$dirEntry.'/'.$dirEntry.".json";
@@ -52,49 +101,81 @@
         }
     }
 
-    function buildCommandsList() {
-        echo "Building commands list ...\n";
-        global $commandsList;
-        $commandsList = [];
-        $dh = opendir(commandsDir);
-        while (($dirEntry = readdir($dh)) !== false) {
-             /* Ignoring some entries */
-             if (in_array($dirEntry, array(".", "..")))
-                continue;
+    // function buildCommandsList() {
+    //     echo "Building commands list ...\n";
+    //     global $commandsList;
+    //     $commandsList = [];
+    //     $dh = opendir(commandsDir);
+    //     while (($dirEntry = readdir($dh)) !== false) {
+    //          /* Ignoring some entries */
+    //          if (in_array($dirEntry, array(".", "..")))
+    //             continue;
 
-            $fullPath = commandsDir.'/'.$dirEntry;
-            if (!file_exists($fullPath)) {
-                echo "- ".$dirEntry.": path access ERROR\n";
-                echo "  ".$fullPath."\n";
-                continue;
-            }
+    //         $fullPath = commandsDir.'/'.$dirEntry;
+    //         if (!file_exists($fullPath)) {
+    //             echo "- ".$dirEntry.": path access ERROR\n";
+    //             echo "  ".$fullPath."\n";
+    //             continue;
+    //         }
 
-            $commandsList[$dirEntry] = $fullPath;
-        }
-    }
+    //         $commandsList[$dirEntry] = $fullPath;
+    //     }
+    // }
 
     /* If JSON name not given on cmd line, parsing all
        devices & commands */
     buildDevicesList();
-    buildCommandsList();
+    // buildCommandsList();
 
     // echo "devl=".json_encode($devicesList)."\n";
-    echo "Checking devices\n";
-    $errors = 0;
+    echo "Checking devices ...\n";
     foreach ($devicesList as $entry => $fullPath) {
         echo "- ".$entry."\n";
         $jsonContent = file_get_contents($fullPath);
         $content = json_decode($jsonContent, true);
         if (json_last_error() != JSON_ERROR_NONE) {
-            echo "  ERROR: Corrupted JSON file\n";
-            $errors++;
+            newDevError($entry, 'ERROR', 'Corrupted JSON file');
+            continue;
         }
         checkDevice($entry, $content);
     }
-    if ($errors != 0)
-        echo "= WARNING: Some corrupted devices found\n";
-    if ($missingCmds != 0)
-        echo "= WARNING: Some missing commands found\n";
-    if (($errors == 0) && ($missingCmds == 0))
+
+    echo "\nChecking used commands ...\n";
+    foreach ($commandsList as $entry => $fullPath) {
+        echo "- ".$entry."\n";
+        $jsonContent = file_get_contents($fullPath);
+        $content = json_decode($jsonContent, true);
+        if (json_last_error() != JSON_ERROR_NONE) {
+            newCmdError($entry, 'ERROR', 'Corrupted JSON file');
+            continue;
+        }
+        checkCommand($entry, $content);
+    }
+
+    echo "\n";
+    if (sizeof($devErrors) != 0 ) {
+        echo "Devices errors summary\n";
+        $f = "";
+        foreach ($devErrors as $e) {
+            if ($f != $e['file']) {
+                echo "- ".$e['file']."\n";
+                $f = $e['file'];
+            }
+            echo "  ".$e['type'].": ".$e['msg']."\n";
+        }
+    } else
         echo "= Devices ok\n";
+
+    if (sizeof($cmdErrors) != 0 ) {
+        echo "Commands errors summary\n";
+        $f = "";
+        foreach ($cmdErrors as $e) {
+            if ($f != $e['file']) {
+                echo "- ".$e['file']."\n";
+                $f = $e['file'];
+            }
+            echo "  ".$e['type'].": ".$e['msg']."\n";
+        }
+    } else
+        echo "= Commands ok\n";
 ?>

c478a8f4ef893fb64120bdb8895a927fa15c0647
Fabrice Charpentier <fabrice.charpentier@st.com>
2021-05-18 18:04:05 +0200

    create_branch update + changelog format rst

--- .tools/update_changelog.sh
+++ .tools/update_changelog.sh
@@ -2,41 +2,60 @@
 
 # Update changelog to add version
 
-CL=docs/fr_FR/changelog.md
-TMP=docs/fr_FR/changelog.md.tmp
+CL=docs/fr_FR/changelog.rst
+TMP=docs/fr_FR/changelog.rst.tmp
 
 echo "Updating changelog..."
 
 VERSION=`cat plugin_info/Abeille.version | tail -1`
 STEP=0
 cat ${CL} |
-while read -r L
+while IFS= read -r L
 do
-    # Checking if any changes added.
-    #   If "# xxx" is followed by "# yyy" then no change
-    #   If "# xxx" is followed by "- yyy" then version to be added
-    
-    if [[ ${L} != "# "* ]] && [[ ${L} != "- "* ]]; then
-        echo "${L}" >> ${TMP}
-        continue
-    fi
-    
+    # ChangeLog
+    # =========
+    #
+    # - JSON: Correction setReportTemp (#1918).
+
+    # ChangeLog
+    # =========
+    #
+    # VERSION X
+    # ---------
+    #
+    # - JSON: Correction setReportTemp (#1918).
+
     if [ ${STEP} -eq 0 ]; then
         echo "${L}" >> ${TMP}
-        STEP=1
-    elif [ ${STEP} -eq 1 ]; then
-        if [[ ${L} == "# "* ]]; then
-            echo "- There is already a version in changelog. Doing nothing"
-            echo
-            rm ${TMP}
-            break
+        if [[ ${L} == "==="* ]]; then
+            STEP=1 # Title found
         fi
-        # It's a list. Need to add version title (# VERSION)
-        echo "# ${VERSION}" >> ${TMP}
-        echo "" >> ${TMP}
-        echo "${L}" >> ${TMP}
-        STEP=2
-    else
+    elif [ ${STEP} -eq 1 ]; then
+        if [[ "${L}" == "- "* ]]; then
+            # It's a list (starts with '- '). Need to add version title (# VERSION)
+            echo "${VERSION}" >> ${TMP}
+            echo "----------" >> ${TMP}
+            echo "" >> ${TMP}
+            echo "${L}" >> ${TMP}
+        else
+            # remove trailing whitespace characters
+            L="${L%"${L##*[![:space:]]}"}"
+            if [ "${L}" == "" ]; then
+                echo >> ${TMP}
+                continue
+            fi
+            # Assuming 'VERSION' line + '------'
+            if [[ "${L}" == *"${VERSION}"* ]]; then
+                echo "- There is already correct version in changelog. Doing nothing"
+                echo
+                rm ${TMP}
+                break
+            fi
+            # Adding current version
+            echo "${VERSION}, ${L}" >> ${TMP}
+        fi
+        STEP=2 # Copy all remaining lines
+    else # STEP==2
         echo "${L}" >> ${TMP}
     fi
 done

024aad4be9a8f303aa22b864a371199933196d96
Tcharp38 <fabrice.charpentier@laposte.net>
2021-08-08 11:12:56 +0200

    AbeilleCmd internal optimization

--- core/php/AbeilleDownload.php
+++ core/php/AbeilleDownload.php
@@ -1,11 +1,11 @@
 <?php
-
 	try {
 		require_once __DIR__ . '/../../../../core/php/core.inc.php';
 		include_file('core', 'authentification', 'php');
 		if (!isConnect() && !jeedom::apiAccess(init('apikey'))) {
 			throw new Exception(__('401 - Accès non autorisé', __FILE__));
 		}
+
 		$pathfile = calculPath(urldecode(init('pathfile')));
 		$pathfile = realpath($pathfile);
 		if ($pathfile === false) {

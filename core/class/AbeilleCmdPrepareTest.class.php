7a9d0677ef10b2fbe1ab078cec4ddf85fa107e46
KiwiHC16 <kiwihc16@free.fr>
2022-01-11 00:01:47 +0100

    Command OnOff in prepareCmd

--- core/class/AbeilleCmdPrepareTest.class.php
+++ core/class/AbeilleCmdPrepareTest.class.php
@@ -22,6 +22,18 @@ class AbeilleCmdPrepareTest extends TestCase {
         $this->assertSame(  $result, json_encode(AbeilleCmdPrepare::proper_parse_str($str)) );
     }
 
+    function prepareCmdTest() {
+        $message = (object) array( 
+            "topic"      => "CmdAbeille1/81F6/OnOff",
+            "payload"    => "Action=On&EP=01",
+            "priority"   => 3,
+        );
+        
+        $result = '{"onoff":"1","dest":"Abeille1","priority":3,"addressMode":"02","address":"81F6","destinationEndpoint":"01","action":"01"}';
+
+        $this->assertSame( $result, json_encode(AbeilleCmdPrepare::prepareCmd($message, 1)) );
+    }
+
 }
 
 

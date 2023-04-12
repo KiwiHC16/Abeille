a7b2bb13cbb6db527d02eb34656512a73fcd4ef0
Tcharp38 <tcharp38@laposte.net>
2023-04-12 18:05:50 +0200

    Xiaomi 4C type support

--- core/php/AbeilleParser-Xiaomi.php
+++ core/php/AbeilleParser-Xiaomi.php
@@ -3,13 +3,13 @@
     // Xiaomi specific parser functions
     // Included by 'AbeilleParser.php'
 
-    // WORK ONGOING. Not all functions migrated their yet.
-
     // Model syntax reminder for Xiaomi
     // "xiaomi": {
     //     "fromDevice": {
-    //         "XX-YY": { "func": "number", "info": "info_cmd_logic_id" }
-    //         "XX-Y2": { "func": "numberDiv", "div": 100, "info": "info_cmd_logic_id" }
+    //         "<clust>-<attr>": {
+    //             "XX-YY": { "func": "number", "info": "info_cmd_logic_id" }
+    //             "XX-Y2": { "func": "numberDiv", "div": 100, "info": "info_cmd_logic_id" }
+    //         }
     //     }
     // }
     // where 'XX'=tag value, 'YY'=data type
@@ -20,27 +20,75 @@
     //              numberMult: resulting number is multiplied by 'mult' before Jeedom update
     //              toPercent: resulting number is converted to percentage (with 'min' & 'max') before Jeedom update
     //              divAndPercent: resulting number is divided ('div') then converted to percentage (with 'min' & 'max') before Jeedom update
+    // Example
+    // "xiaomi": {
+    //     "fromDevice": {
+    //         "0000-FF02": {
+    //             "01-21": {
+    //                 "func": "numberDiv",
+    //                 "div": 1000,
+    //                 "info": "0001-01-0020",
+    //                 "comment": "Battery-Volt"
+    //             }
+    //         }
+    //     }
+    // }
+
+    function xiaomiDecodeFunction($valueHex, $value, $m, $map, &$attrReportN = null, &$toMon = null) {
+        $value2 = $value;
+        $mapTxt = ' ==> ';
+
+        $func = (isset($map['func']) ? $map['func'] : "raw");
+        if ($func == "raw") {
+            $value2 = $valueHex;
+        } else if ($func == "numberDiv") {
+            $div = $map['div'];
+            $value2 = $value / $div;
+            $mapTxt .= 'div='.$div.', ';
+        } else if ($func == "numberMult") {
+            $mult = $map['mult'];
+            $value2 = $value * $mult;
+            $mapTxt .= 'mult='.$mult.', ';
+        } else if ($func == "toPercent") {
+            $min = (isset($map['min']) ? $map['min'] : 2.85);
+            $max = (isset($map['max']) ? $map['max'] : 3.0);
+            if ($value > $max)
+                $value2 = $max;
+            else if ($value < $min)
+                $value2 = $min;
+            $value2 = ($value2 - $min) / ($max - $min);
+            $value2 *= 100;
+            $mapTxt .= 'min='.$min.', max='.$max.', ';
+        } else if ($func == "divAndPercent") {
+            $div = (isset($map['div']) ? $map['div'] : 1);
+            $value2 /= $div;
+            $min = (isset($map['min']) ? $map['min'] : 2.85);
+            $max = (isset($map['max']) ? $map['max'] : 3.0);
+            if ($value2 > $max)
+                $value2 = $max;
+            else if ($value2 < $min)
+                $value2 = $min;
+            $value2 = ($value2 - $min) / ($max - $min);
+            $value2 *= 100;
+            $mapTxt .= 'div='.$div.', min='.$min.', max='.$max.', ';
+        }
+        $mapTxt .= $map['info'];
+        $mapTxt .= '='.$value2;
+
+        $m .= ' => '.$value.$mapTxt;
+        parserLog('debug', $m);
+
+        if ($toMon !== null)
+            $toMon[] = $m;
+        if ($attrReportN !== null)
+            $attrReportN[] = array( "name" => $map['info'], "value" => $value2 );
+    }
 
     // Decode Xiaomi tags
     // Based on https://github.com/dresden-elektronik/deconz-rest-plugin/wiki/Xiaomi-manufacturer-specific-clusters%2C-attributes-and-attribute-reporting
     // Could be cluster 0000, attr FF01, type 42 (character string)
     // Could be cluster FCC0, attr 00F7, type 41 (octet string)
     function xiaomiDecodeTags($net, $addr, $clustId, $attrId, $pl, &$attrReportN = null, &$toMon = null) {
-        // $tagsList = array(
-        //     "01-21" => array( "desc" => "Battery-Volt" ),
-        //     "03-28" => array( "desc" => "Device-Temp" ),
-        //     "0A-21" => array( "desc" => "Parent-Addr" ),
-        //     "0B-21" => array( "desc" => "Light-Level" ),
-        //     "64-10" => array( "desc" => "OnOff" ), // Or Open/closed
-        //     "64-29" => array( "desc" => "Temperature" ),
-        //     "65-21" => array( "desc" => "Humidity" ),
-        //     "66-2B" => array( "desc" => "Pressure" ),
-        //     "95-39" => array( "desc" => "Current" ), // Or Consumption
-        //     "96-39" => array( "desc" => "Voltage" ),
-        //     "97-39" => array( "desc" => "Consumption" ),
-        //     "98-39" => array( "desc" => "Power" ),
-        // );
-
         $eq = &getDevice($net, $addr); // By ref
         // parserLog('debug', 'eq='.json_encode($eq));
         if (!isset($eq['xiaomi']) || !isset($eq['xiaomi']['fromDevice'][$clustId.'-'.$attrId])) {
@@ -69,78 +117,35 @@
                 $valueHex = AbeilleTools::reverseHex($valueHex);
             $value = AbeilleParser::decodeDataType($valueHex, $typeId, false, 0, $dataSize, $valueHex);
 
+            $m = '    Tag='.$tagId.', Type='.$typeId.'/'.$type['short'];
+
             $idx = strtoupper($tagId.'-'.$typeId);
-            // if (isset($tagsList[$idx]))
-            //     $tagName = '/'.$tagsList[$idx]['desc'];
-            // else
-            //     $tagName = '';
-            // parserLog('debug', '  Tag='.$tagId.$tagName.', Type='.$typeId.'/'.$type['short'].', Value='.$valueHex.' => '.$value);
-            if (isset($mapping[$idx])) {
-                $map = $mapping[$idx];
-                $value2 = $value;
-                $mapTxt = ' ==> ';
-
-                $func = (isset($map['func']) ? $map['func'] : "raw");
-                if ($func == "raw") {
-                    $value2 = $valueHex;
-                } else if ($func == "numberDiv") {
-                    $div = $map['div'];
-                    $value2 = $value / $div;
-                    $mapTxt .= 'div='.$div.', ';
-                } else if ($func == "numberMult") {
-                    $mult = $map['mult'];
-                    $value2 = $value * $mult;
-                    $mapTxt .= 'mult='.$mult.', ';
-                } else if ($func == "toPercent") {
-                    $min = (isset($map['min']) ? $map['min'] : 2.85);
-                    $max = (isset($map['max']) ? $map['max'] : 3.0);
-                    if ($value > $max)
-                        $value2 = $max;
-                    else if ($value < $min)
-                        $value2 = $min;
-                    $value2 = ($value2 - $min) / ($max - $min);
-                    $value2 *= 100;
-                    $mapTxt .= 'min='.$min.', max='.$max.', ';
-                } else if ($func == "divAndPercent") {
-                    $div = (isset($map['div']) ? $map['div'] : 1);
-                    $value2 /= $div;
-                    $min = (isset($map['min']) ? $map['min'] : 2.85);
-                    $max = (isset($map['max']) ? $map['max'] : 3.0);
-                    if ($value2 > $max)
-                        $value2 = $max;
-                    else if ($value2 < $min)
-                        $value2 = $min;
-                    $value2 = ($value2 - $min) / ($max - $min);
-                    $value2 *= 100;
-                    $mapTxt .= 'div='.$div.', min='.$min.', max='.$max.', ';
-                }
-                $mapTxt .= $map['info'];
-                $mapTxt .= '='.$value2;
-
-                if ($attrReportN !== null)
-                    $attrReportN[] = array( "name" => $map['info'], "value" => $value2 );
-            } else
-                $mapTxt = ' (ignored)';
-            $m = '    Tag='.$tagId.', Type='.$typeId.'/'.$type['short'].', ValueHex='.$valueHex.' => '.$value.$mapTxt;
-            parserLog('debug', $m);
-            if ($toMon !== null)
+            if (isset($mapping[$idx]))
+                xiaomiDecodeFunction($valueHex, $value, $m, $mapping[$idx], $attrReportN, $toMon);
+            else {
+                $m .= ' => '.$value.' (ignored)';
+                parserLog('debug', $m);
                 $toMon[] = $m;
-
+            }
         }
     }
 
     function xiaomiReportAttributes($net, $addr, $clustId, $pl, &$attrReportN = null, &$toMon = null) {
+        $eq = &getDevice($net, $addr); // By ref
         $l = strlen($pl);
         for ( ; $l > 0; ) {
             $attrId = substr($pl, 2, 2).substr($pl, 0, 2);
             $attrType = substr($pl, 4, 2);
-            $pl = substr($pl, 6);
+            $pl = substr($pl, 6); // Skipping attr ID + attr type
             $pl2 = $pl;
 
             // Computing attribute size for legacy decodes
             if (($attrType == "41") || ($attrType == "42")) {
                 $attrSize = hexdec(substr($pl, 0, 2));
-                $pl = substr($pl, 2); // Skip 1B size
+                $pl = substr($pl, 2); // Skipping 1B size
+            } else if ($attrType == "4C") { // Struct
+                $pl = substr($pl, 4); // Skipping 2B count
+                $attrSize = strlen($pl) / 2; // WARNING: This is wrong. Then can't support consecutive attributes
             } else {
                 $type = zbGetDataType($attrType);
                 $attrSize = $type['size'];
@@ -155,34 +160,10 @@
             $l = strlen($pl);
 
             //
-            // Legacy decoding
-            //
-
-            // No longer required as soon as 'xiaomi' JSON field is there
-
-            // Xiaomi door sensor V2
-            // if (($attrId == "FF01") && ($attrSize == 29)) {
-            //     // Assuming $dataType == "42"
-
-            //     parserLog('debug', '  Xiaomi proprietary (Door Sensor)');
-            //     $attrReportN = [];
-            //     xiaomiDecodeTags($net, $addr, $clustId, $attrId, $attrData, $attrReportN, $toMon);
-            //     continue;
-            // }
-
-            // Xiaomi temp/humidity/pressure square sensor
-            // else if (($attrId == 'FF01') && ($attrSize == 37)) {
-            //     // Assuming $dataType == "42"
-
-            //     parserLog('debug', '  Xiaomi proprietary (Temp square sensor)');
-            //     $attrReportN = [];
-            //     xiaomiDecodeTags($net, $addr, $clustId, $attrId, $attrData, $attrReportN, $toMon);
-            //     continue;
-            // }
-
-            //
-            // New decoding
-            //
+            // Flexible decoding according to 'xiaomi' model's section
+            // - Attribute
+            // - Attribute including tags
+            // - Attribute type struct 4C
             /* "xiaomi": {
                    "fromDevice": {
                         "FCC0-0112": {
@@ -195,11 +176,18 @@
                                 "info": "0001-01-0020",
                                 "comment": "Battery volt"
                             }
+                        },
+                        "FCC0-00F7": {
+                            "10": {
+                                "func": "numberDiv",
+                                "div": 1000,
+                                "info": "0001-01-0020",
+                                "comment": "Battery volt"
+                            }
                         }
                     }
                 } */
 
-            $eq = &getDevice($net, $addr); // By ref
             if (isset($eq['xiaomi']) && isset($eq['xiaomi']['fromDevice'][$clustId.'-'.$attrId])) {
                 $fromDev = $eq['xiaomi']['fromDevice'][$clustId.'-'.$attrId];
                 if (isset($fromDev['info'])) {
@@ -215,18 +203,36 @@
                         .', ValueHex='.$valueHex.' => '.$value.' ==> '.$fromDev['info'].'='.$value;
                     parserLog('debug', $m);
                     $toMon[] = $m;
-                } else {
+                } else if (strlen(array_key_first($fromDev)) == 5) { // Idx format 'TA-TY', TA=tagId, TY=typeId
                     // 'CLUSTER-ATTRIB' + 'TAG-TYPE' syntax
                     $m = '  AttrId='.$attrId
                         .', AttrType='.$attrType;
                     parserLog('debug', $m);
                     $toMon[] = $m;
-                    if ($attrType == '4C') {
-                        // 4C/Struct specific support => treating like 42
-                        $a = substr($attrData, 4); // Skipping len
-                        xiaomiDecodeTags($net, $addr, $clustId, $attrId, $a, $attrReportN, $toMon);
-                    } else
-                        xiaomiDecodeTags($net, $addr, $clustId, $attrId, $attrData, $attrReportN, $toMon);
+
+                    xiaomiDecodeTags($net, $addr, $clustId, $attrId, $attrData, $attrReportN, $toMon);
+                } else { // CLUSTER-ATTRIB for type 4C/struct
+                    $m = '  AttrId='.$attrId
+                        .', AttrType='.$attrType;
+                    parserLog('debug', $m);
+                    $toMon[] = $m;
+
+                    // Note: 4B count already skipped
+                    while (strlen($attrData) > 0) {
+                        $t = substr($attrData, 2);
+                        $attrData = substr($attrData, 2); // Skipping type
+                        $m = '    SubType='.$t.', ValueHex='.$valueHex;
+                        $value = AbeilleParser::decodeDataType($attrData, $t, true, 0, $s, $valueHex);
+                        if (isset($fromDev[$t]))
+                            xiaomiDecodeFunction($valueHex, $value, $m, $fromDev[$t], $attrReportN, $toMon);
+                        else {
+                            $m .= ' => '.$value.' (ignored)';
+                            parserLog('debug', $m);
+                            $toMon[] = $m;
+                        }
+
+                        $attrData = substr($attrData, $s * 2); // Skipping data
+                    }
                 }
                 continue;
             }

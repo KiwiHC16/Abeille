651d55b5609eaf485cc7b3faf47cd3d146d02fae
Tcharp38 <tcharp38@laposte.net>
2023-05-03 14:24:27 +0200

    Network map

--- desktop/modal/AbeilleNetworkMap.modal.php
+++ desktop/modal/AbeilleNetworkMap.modal.php
@@ -25,45 +25,38 @@
 
     define('maxLevels', 10); // Number of levels
     sendVarToJS('maxLevels', maxLevels);
-
-    function addSelect() {
-        echo '<select>';
-        echo '</select>';
-    }
 ?>
 {{Cette page vous permet de définir les niveaux et plans associés}}.<br><br>
 
-<form>
-    <div class="col-sm-6">
-        <table>
-            <tr>
-                <th style="width:150px">{{Niveau}}</th>
-                <th style="width:300px">{{Plan}}</th>
-            </tr>
-            <?php
-            for ($t = 0; $t < maxLevels; $t++) {
-                echo "<tr>";
-                echo "<td>";
-                addSelect("idSelect".$t);
-                echo "</td>";
-                echo "<td></td>";
-                echo "</tr>";
-            }
-            ?>
-        </table>
-    </div>
-    <div class="col-sm-6">
-        {{Ajouter niveau}}:
-        <input id="idLevelNew" type="text" value="">
-        <button id="idLevelPlus" class="btn btn-success btn-sm" onclick="levelAdd()">+</button>
-    </div>
-</form>
+<table>
+    <tr>
+        <th style="width:150px">{{Niveau}}</th>
+        <th style="width:300px">{{Plan}}</th>
+    </tr>
+    <?php
+    for ($t = 0; $t < maxLevels; $t++) {
+        echo "<tr>";
+        echo "<td>";
+        echo '<input id="idLevel-'.$t.'" type="text" value="">';
+        echo "</td>";
+        echo "<td>";
+        echo '<input id="idMap-'.$t.'" type="text" value="">';
+        echo '<button onclick="addNewMap('.$t.')">+</button>';
+        echo "</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
 
-<form>
+<br>
+<button onclick="saveLevels()">{{Sauvegarder}}</button>
+<button onclick="addNewMap('.$t.')">+</button>
+
+<!-- <form>
     <div class="form-group">
         <label class="col-sm-2 control-label">{{Niveau}}</label>
         <div class="col-sm-9">
-            <!-- Drop down list to select Zigate -->
+            <! -- Drop down list to select Zigate -- >
             <select id="idLevel" onchange="levelChanged()" style="width:100px">
             </select>
             <button id="idLevelMin" class="btn btn-danger btn-sm" onclick="levelRemove()">-</button>
@@ -77,7 +70,7 @@
             <button id="map" onclick="uploadMap()">{{Plan}}</button>
         </div>
     </div>
-</form>
+</form> -->
 
 <script>
     // TEMP
@@ -89,54 +82,80 @@
     };
     // END TEMP
 
-    refreshSelects();
+    refreshLevels();
 
-    function refreshSelects() {
-        console.log("refreshSelects(). networkMap=", networkMap);
+    function refreshLevels() {
+        console.log("refreshLevels(). networkMap=", networkMap);
         if (typeof networkMap[zgId] === 'undefined')
             return;
 
         nm = networkMap[zgId];
         console.log("nm=", nm);
-        for (s = 0; s < maxLevels; s++) {
-            select = document.getElementById("idSelect"+s);
-            for (const [level, map] of Object.entries(nm)) {
-                var el = document.createElement("option");
-                el.textContent = level;
-                el.value = map;
-                select.appendChild(el);
-            }
+        s = 0
+        for (const [level, map] of Object.entries(nm)) {
+            elm = document.getElementById("idLevel-"+s);
+            elm.value = level;
+            elm = document.getElementById("idMap-"+s);
+            elm.value = map;
+            s++;
+            if (s == maxLevels)
+                break;
         }
     }
 
-    function levelChanged() {
-        console.log("levelChanged())");
-        level = $("#idLevel").val();
-        console.log("level=", level);
+    // Check & save config
+    function saveLevels() {
+        console.log("saveLevels()");
     }
+
+    // Add new map to index 'idx'
+    function addNewMap(idx) {
+        console.log("addNewMap()");
+
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.png';
+        input.onchange = e => {
+
+            var file = e.target.files[0];
+            // file.name = the file's name including extension
+            // file.size = the size in bytes
+            // file.type = file type ex. 'application/pdf'
+            console.log("file=", file);
+            elm = document.getElementById("idMap-"+idx);
+            elm.value = file.name;
+        }
+        input.click();
+    }
+
+    // function levelChanged() {
+    //     console.log("levelChanged())");
+    //     level = $("#idLevel").val();
+    //     console.log("level=", level);
+    // }
 
     // Remove current selected level
-    function levelRemove() {
-        select = document.getElementById("idLevel");
-        index = select.selectedIndex;
-        console.log("levelRemove(index="+index+")");
-        if (index < 0)
-            return;
-        select.remove(index);
-    }
+    // function levelRemove() {
+    //     select = document.getElementById("idLevel");
+    //     index = select.selectedIndex;
+    //     console.log("levelRemove(index="+index+")");
+    //     if (index < 0)
+    //         return;
+    //     select.remove(index);
+    // }
 
     // Add a new level
-    function levelAdd() {
-        value = document.getElementById("idLevelNew").value;
-        console.log("levelAdd(value="+value+")");
-        if (value == '')
-            return;
+    // function levelAdd() {
+    //     value = document.getElementById("idLevelNew").value;
+    //     console.log("levelAdd(value="+value+")");
+    //     if (value == '')
+    //         return;
 
         // TODO: Ignore if level already exists
 
-        var el = document.createElement("option");
-        el.textContent = value;
-        el.value = value;
-        select.appendChild(el);
-    }
+    //     var el = document.createElement("option");
+    //     el.textContent = value;
+    //     el.value = value;
+    //     select.appendChild(el);
+    // }
 </script>

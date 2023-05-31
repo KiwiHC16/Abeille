d853cb22fb3adb3f5eb4cab9046fb7246b4f5c60
Tcharp38 <fabrice.charpentier@laposte.net>
2023-05-31 23:28:57 +0200

    Power cycle script

---
+++ core/scripts/powerCycleUsb.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+###
+### An attempt to power cycle USB device
+### Tcharp38
+###
+
+FULLDEV=$1
+echo "FULLDEV=$FULLDEV"
+
+# TODO: Removing "/dev/" prefix
+DEV=${FULLDEV#/dev/}
+echo "DEV=$DEV"
+
+# Identifing corresponding port using dmesg
+# [   11.037185] usb 3-1.2: cp210x converter now attached to ttyUSB0
+DMESG=`dmesg | grep "now attached to ${DEV}" | sed 's/^.*\(usb .*\).*$/\1/'`
+if [ "${DMESG}" == "" ]; then
+    exit 3
+fi
+echo "DMESG=$DMESG"
+DMESG2=`echo ${DMESG} | sed 's/:.*//'`
+echo "DMESG2='$DMESG2'"
+PORT=${DMESG2#usb }
+echo "PORT='$PORT'"
+
+# Port identified. Let's do power cycling
+echo "$PORT" > /sys/bus/usb/drivers/usb/unbind
+sleep 2
+echo "$PORT" > /sys/bus/usb/drivers/usb/bind

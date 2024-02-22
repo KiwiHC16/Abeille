cdc8d9e3ef59acfd065bcd6410ef4190a82b16da
Tcharp38 <tcharp38@laposte.net>
2024-02-22 17:09:33 +0100

    Scripts

--- core/scripts/updateFirmware.sh
+++ core/scripts/updateFirmware.sh
@@ -26,7 +26,6 @@ DIN_SCRIPT=${PWD}/../scripts/flash_ZiGate-DIN.py
 # Checks
 echo "- Vérifications préliminaires"
 echo ${PWD}
-error=0
 if [ $# -lt 4 ]; then
     echo "= ERREUR: Argument(s) manquant(s) !"
     echo "=         updateFirmware.sh <action> <zigatePort> <zigateType> <GpioLib> [fwfile]"
@@ -36,57 +35,64 @@ ACTION=$1
 ZGPORT=$2
 ZGTYPE=$3
 GPIOLIB=$4
-if [ ${ZGTYPE} != "PI" ] && [ ${ZGTYPE} != "PIv2" ] && [ ${ZGTYPE} != "DIN" ]; then
-    echo "= ERREUR: Type de Zigate invalide ! (PI, PIv2 ou DIN)"
-    echo "=         updateFirmware.sh <action> <zigatePort> <zigateType> <GpioLib> [fwfile]"
-    exit 1
-fi
-if [ ${ACTION} == "flash" ] && [ $# -lt 5 ]; then
-    echo "= ERREUR: Nom du FW manquant !"
-    echo "=         updateFirmware.sh <action> <zigatePort> <zigateType> <GpioLib> [fwfile]"
-    exit 1
-fi
-FW=$5
-
+FW=""
+ERROR=0
 if [ ${ACTION} != "flash" ] && [ ${ACTION} != "check" ] && [ ${ACTION} != "eraseeeprom" ]; then
     echo "= ERREUR: Action '${ACTION}' non supportée."
     echo "=         Choix=flash/check/eraseeeprom"
-    error=1
-else
-    if [ ${ACTION} == "flash" ]; then
-        if [[ "${FW}" == "/"* ]]; then # Absolut path ?
-            FW_PATH="${FW}"
-        else
-            FW_PATH="${FW_DIR}/${FW}"
-        fi
-        if [ ! -e ${FW_PATH} ]; then
-            echo "= ERREUR: le FW choisi n'existe pas !"
-            echo "=         FW: ${FW_PATH}"
-            error=1
-        fi
+    ERROR=1
+fi
+if [ ! -e ${ZGPORT} ]; then
+    echo "= ERREUR: Le port tty ${ZGPORT} n'existe pas !"
+    ERROR=1
+fi
+if [ ${ZGTYPE} != "PI" ] && [ ${ZGTYPE} != "PIv2" ] && [ ${ZGTYPE} != "DIN" ]; then
+    echo "= ERREUR: Type de Zigate invalide ! (PI, PIv2 ou DIN)"
+    echo "=         updateFirmware.sh <action> <zigatePort> <zigateType> <GpioLib> [fwfile]"
+    ERROR=1
+fi
+if [ ${GPIOLIB} != "WiringPi" ] && [ ${GPIOLIB} != "PiGpio" ]; then
+    echo "= ERREUR: Lib GPIO invalide ! (WiringPi ou PiGpio)"
+    ERROR=1
+fi
+if [ ${ACTION} == "flash" ] && [ $# -lt 5 ]; then
+    echo "= ERREUR: Nom du FW manquant !"
+    echo "=         updateFirmware.sh <action> <zigatePort> <zigateType> <GpioLib> <fwfile>"
+    ERROR=1
+fi
+if [ ${ACTION} == "flash" ]; then
+    if [[ "${FW}" == "/"* ]]; then # Absolut path ?
+        FW_PATH="${FW}"
+    else
+        FW_PATH="${FW_DIR}/${FW}"
     fi
-    if [ ! -e ${ZGPORT} ]; then
-        echo "= ERREUR: le port tty choisi n'existe pas !"
-        echo "=         Port: ${ZGPORT}"
-        error=1
-    fi
-    if [ ${GPIOLIB} == "WiringPi" ]; then
-        command -v gpio >/dev/null
-        if [ $? -ne 0 ]; then
-            echo "= ERREUR: Commande 'gpio' manquante ou non exécutable !"
-            echo "=         Le package WiringPi est probablement mal installé."
-            error=1
-        fi
-    # elif [ ${GPIOLIB} == "PiGpio" ]; then
-    # else
-    elif [ ${GPIOLIB} != "PiGpio" ]; then
-        echo "= ERREUR: Type de lib GPIO invalid (${GPIOLIB}) !"
-        error=1
+    if [ ! -e ${FW_PATH} ]; then
+        echo "= ERREUR: le FW choisi n'existe pas !"
+        echo "=         FW: ${FW_PATH}"
+        ERROR=1
     fi
 fi
-if [ $error -ne 0 ]; then
-    exit 1
+if [ $ERROR -ne 0 ]; then
+    exit $ERROR
 fi
+
+if [ ${GPIOLIB} == "WiringPi" ]; then
+    command -v gpio >/dev/null
+    if [ $? -ne 0 ]; then
+        echo "= ERREUR: Commande 'gpio' manquante ou non exécutable !"
+        echo "=         Le package WiringPi est probablement mal installé."
+        ERROR=2
+    fi
+# elif [ ${GPIOLIB} == "PiGpio" ]; then
+# else
+elif [ ${GPIOLIB} != "PiGpio" ]; then
+    echo "= ERREUR: Type de lib GPIO invalid (${GPIOLIB}) !"
+    ERROR=2
+fi
+if [ $ERROR -ne 0 ]; then
+    exit $ERROR
+fi
+
 echo "= Ok"
 
 if [ ${ZGTYPE} == "PI" ]; then
@@ -98,11 +104,11 @@ if [ ${ZGTYPE} == "PI" ]; then
     sudo make
     if [ $? -ne 0 ]; then
         echo "= ERREUR: Compilation ratée !"
-        error=2
+        ERROR=3
     fi
     # echo "= ERREUR: Programmateur Jennic manquant !"
     # echo "=         ${PROG_PIv1}"
-    # error=1
+    # ERROR=1
     popd >/dev/null
     if [ ! -x ${PROG_PIv1} ]; then
         # Attempting to correct execution right
@@ -111,7 +117,7 @@ if [ ${ZGTYPE} == "PI" ]; then
     if [ ! -x ${PROG_PIv1} ]; then
         echo "= ERREUR: Le programmateur Jennic n'est pas exécutable !"
         echo "=         ${PROG_PIv1}"
-        error=2
+        ERROR=3
     fi
 elif [ ${ZGTYPE} == "PIv2" ]; then
     PROG=${PROG_PIv2}
@@ -122,14 +128,14 @@ elif [ ${ZGTYPE} == "DIN" ]; then
     if [ $? -ne 0 ]; then
         echo "= ERREUR: 'python3' manquant ou non exécutable !"
         echo "=         Le package 'python3' est probablement mal installé."
-        error=2
+        ERROR=3
     fi
 else
     echo "= ERREUR: Type ${ZGTYPE} inconnu !"
-    error=2
+    ERROR=3
 fi
-if [ $error != 0 ]; then
-    exit $error
+if [ $ERROR -ne 0 ]; then
+    exit $ERROR
 fi
 
 # Si check seulement on quitte ici
@@ -152,13 +158,13 @@ echo "  Prog    : ${PROG}"
 # Mode production: FLASH=1, RESET=0 puis 1
 # Mode flash: FLASH=0, RESET=0 puis 1
 if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
+    echo "- Basculement de la PiZiGate en mode 'flash'"
     if [ ${GPIOLIB} == "WiringPi" ]; then
         gpio mode 0 out
         gpio mode 2 out
 
         # Passage en mode 'flash'
         gpio write 2 0
-        sleep 1
         gpio write 0 0
         sleep 1
         gpio write 0 1
@@ -173,33 +179,34 @@ if [ ${ACTION} == "eraseeeprom" ]; then
     sudo ${PROG} -V 6 -P 115200 -v --erase -s ${ZGPORT} 2>&1
     if [ $? != 0 ]; then
         echo "= ERREUR: Effacement impossible"
-        status=2
+        ERROR=2
     else
         echo "= Ok. Effacement terminé"
-        echo "- Redémarrage de la PiZiGate"
-        status=0
     fi
 else
-    if [ ${ZGTYPE} == "DIN" ]; then
+    if [ ${ZGTYPE} == "PI" ]; then
+        sudo ${PROG} -V 6 -P 115200 -v -f ${FW_PATH} -s ${ZGPORT} 2>&1
+    elif [ ${ZGTYPE} == "PIv2" ]; then
+        sudo ${PROG} -V 3 -s ${ZGPORT} -P 115200 -Y -p ${FW_PATH} 2>&1
+    elif [ ${ZGTYPE} == "DIN" ]; then
         sudo python3 ${DIN_SCRIPT} -s ${ZGPORT} -b 250000 -f ${FW_PATH} 2>&1
     else # PI or PIv2
-        sudo ${PROG} -V 6 -P 115200 -v -f ${FW_PATH} -s ${ZGPORT} 2>&1
+        echo "ERREUR: Type ${ZGTYPE} non supporté !"
+        exit 3
     fi
     if [ $? != 0 ]; then
         # echo "= ERREUR: Programmation impossible"
-        status=2
+        ERROR=3
     else
         echo "= Ok. Programmation faite"
-        echo "- Redémarrage de la PiZiGate"
-        status=0
     fi
 fi
 
 # Switch back to 'prod' mode
 if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
+    echo "- Redémarrage de la PiZiGate"
     if [ ${GPIOLIB} == "WiringPi" ]; then
         gpio write 2 1
-        sleep 1
         gpio write 0 0
         sleep 1
         gpio write 0 1
@@ -209,10 +216,10 @@ if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
 fi
 
 # Final status
-if [ $status  -eq 0 ]; then
+if [ $ERROR  -eq 0 ]; then
     echo "= Tout s'est bien passé. Vous pouvez fermer ce log."
 else
     echo "= ATTENTION !!! "
     echo "= Quelque chose s'est mal passé. Veuillez vérifier le log ci-dessus."
 fi
-exit $status
+exit $ERROR

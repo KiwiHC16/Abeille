15b81a2dddd5d7288401459a8e67d0bca1b44eea
Tcharp38 <tcharp38@laposte.net>
2024-02-22 14:31:05 +0100

    dimpZigate script

--- core/scripts/dumpZigate.sh
+++ core/scripts/dumpZigate.sh
@@ -19,7 +19,7 @@ ZGPORT=$1
 ZGTYPE=$2
 GPIOLIB=$3
 DUMPFILE=$4
-if [ ${ZGTYPE} != "PIv2" ]; then
+if [ ${ZGTYPE} != "PI" ] && [ ${ZGTYPE} != "PIv2" ]; then
     echo "= ERREUR: Type de Zigate invalide ! (PIv2 seulement)"
     echo "=         dumpZigate.sh <zigatePort> <zigateType> <gpioLib> <dumpFile>"
     exit 1
@@ -33,16 +33,21 @@ if [ ${GPIOLIB} == "WiringPi" ]; then
         ERROR=1
     fi
 fi
+if [ ! -e ${ZGPORT} ]; then
+    echo "= ERREUR: Le port ${ZGPORT} n'existe pas !"
+    ERROR=1
+fi
+
 if [ $ERROR -ne 0 ]; then
     exit $ERROR
 fi
 
-echo "Dumping Zigate content"
-echo "  Port tty: ${ZGPORT}"
-echo "  Type    : ${ZGTYPE}"
-echo "  Lib GPIO: ${GPIOLIB}"
-echo "  File    : ${DUMPFILE}"
-#echo "  Prog    : ${PROG}"
+# Quick check that Zigate port is free
+FIELDS=`sudo lsof -Fcn ${ZGPORT}`
+if [ "${FIELDS}" != "" ]; then
+    echo "= ERREUR: Le port ${ZGPORT} ne semble pas libre. Assurez vous que rien ne l'utilise !"
+    exit 2
+fi
 
 # PiZigate reminder
 # port 0 = RESET
@@ -50,6 +55,7 @@ echo "  File    : ${DUMPFILE}"
 # Mode production: FLASH=1, RESET=0 puis 1
 # Mode flash: FLASH=0, RESET=0 puis 1
 if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
+    echo "Configuring PI Zigate GPIOs for flash mode"
     if [ ${GPIOLIB} == "WiringPi" ]; then
         gpio mode 0 out
         gpio mode 2 out
@@ -66,13 +72,27 @@ if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
     fi
 fi
 
-if [ ${ZGTYPE} == "PIv2" ]; then
-    ${DK6PROG} -V 3 -s ${ZGPORT} -P 500000 -d FLASH:0x9FFFF@0=${DUMPFILE}
+echo "Dumping Zigate content"
+echo "- Port tty: ${ZGPORT}"
+echo "- Type    : ${ZGTYPE}"
+echo "- Lib GPIO: ${GPIOLIB}"
+echo "- File    : ${DUMPFILE}"
+#echo "  Prog    : ${PROG}"
+echo
+
+if [ ${ZGTYPE} == "PI" ]; then
+    # JN5168: Flash appli from 0x00080000 to 0x000C0000
+    # For test only. DK6 seems to not support JN5168
+    ${DK6PROG} -V 3 -s ${ZGPORT} -P 115200 -d FLASH:0xC0000@0x80000=${DUMPFILE}
+elif [ ${ZGTYPE} == "PIv2" ]; then
+    # JN5189: Flash area (640KB) from 0x0000_0000 to 0x0009_FFFF
+    ${DK6PROG} -V 3 -s ${ZGPORT} -P 115200 -d FLASH:0x9FFFF@0=${DUMPFILE}
 fi
 ERROR=$?
 
 # Switch back to 'prod' mode
 if [ ${ZGTYPE} == "PI" ] || [ ${ZGTYPE} == "PIv2" ]; then
+    echo "Restoring PI Zigate GPIOs config for prod mode"
     if [ ${GPIOLIB} == "WiringPi" ]; then
         gpio write 2 1
         sleep 1

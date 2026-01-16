c77699d67758a67c99560e542d927836815df3c7
Tcharp38 <tcharp38@laposte.net>
2026-01-16 15:52:31 +0100

    Color inspector prelim update

--- core/php/AbeilleColorInspector.php
+++ core/php/AbeilleColorInspector.php
@@ -46,7 +46,14 @@
             $msgToCli = array('type' => 'ERROR');
         global $messages;
         $messages[] = $msgToCli;
-        // echo json_encode($msgToCli);
+    }
+
+    function msgToCli2($type, $value, $value2 = '') {
+        if ($type == "step")
+            $msgToCli = array('type' => $type, 'name' => $value, 'status' => $value2);
+        else
+            $msgToCli = array('type' => 'ERROR');
+        echo json_encode($msgToCli);
     }
 
     function saveEqConfig($eqLogic, $key, $value) {
@@ -88,9 +95,16 @@
         }
 
         // Next steps ??
-        // Supported server attributes ?
-        // Configure reporting ok on ?
-        // Test
+        // Supported server attributes ? => Where to store them if this process is recalled multiple times ?
+
+        // Try moveToColor (X/Y)
+        // Auto report ? or read back ? => 0300-EP-0007
+        // Same question after configure reporting done
+        // Same question after bind done
+
+        // Try set color temp (moveToColorTemp)
+        // Auto report ? or read back ? => 0300-EP-0007
+        // Same question after configure reporting done
     }
 
     /*--------------------------------------------------------------------------------------------------*/
@@ -99,6 +113,26 @@
 
     logSetConf(jeedom::getTmpFolder("Abeille")."/AbeilleColorInspector.log", true);
     logMessage("", ">>> AbeilleColorInspector starting");
+
+    // if (!isset($_POST['eqId'])) {
+    //     $msgToCli = array('type' => 'error', 'errMsg' => 'Missing equipment ID');
+    //     echo json_encode($msgToCli);
+    //     logMessage("", "<<< AbeilleColorInspector exiting on error.");
+    //     exit(1);
+    // }
+
+    // $eqId = $_POST['eqId'];
+    // $eqLogic = Abeille::byId($eqId);
+    // if (!is_object($eqLogic)) {
+    //     msgToCli("error", "Unknown device ID ${eqId}");
+    //     exit(2);
+    // }
+
+    // $queueLQIToCmd = msg_get_queue($abQueues["xToCmd"]["id"]);
+
+    // $messages = [];
+    // inspectDevice($eqId, $eqLogic);
+    // echo json_encode($messages);
 
     if (!isset($_POST['eqId'])) {
         $msgToCli = array('type' => 'error', 'errMsg' => 'Missing equipment ID');
@@ -116,9 +150,49 @@
 
     $queueLQIToCmd = msg_get_queue($abQueues["xToCmd"]["id"]);
 
-    $messages = [];
-    inspectDevice($eqId, $eqLogic);
-    echo json_encode($messages);
+    $eqLogicId = $eqLogic->getLogicalId();
+    list($net, $addr) = explode("/", $eqLogicId);
+    $exitCI = false;
+    while (!$exitCI) {
+        $zigbee = $eqLogic->getConfiguration('ab::zigbee', []);
+        if ($zigbee == []) { // TODO: How to react if no zigbee ? Repair ?
+            msgToCli2("step", "No 'zigbee' info => Repair required.");
+            break;
+        }
+
+        // Color cluster supported ?
+        $colorSupportedEpId = false; // Color cluster supported on EP, or false
+        foreach ($zigbee['endPoints'] as $epId2 => $ep2) {
+            if (!isset($ep2['servClusters'])) {
+                msgToCli2("step", "No 'zigbee/$epId2/servClusters' info => Repair required.");
+                $exitCI = true;
+                break;
+            }
+            // servClusters => Supported server clusters separated by '/': ex '0000/0003/FFFF/0006'
+            if (strpos($ep2['servClusters'], '0300') === false) {
+                logMessage('debug', "  Color cluster NOT supported on EP $epId2");
+                continue;
+            }
+            $colorSupportedEpId = $epId2;
+        }
+        if ($colorSupportedEpId === false) {
+            msgToCli2("step", "No color cluster support for this device.");
+            $exitCI = true;
+            break;
+        }
+
+        // Next steps ??
+        // Supported server attributes ? => Where to store them if this process is recalled multiple times ?
+
+        // Try moveToColor (X/Y)
+        // Auto report ? or read back ? => 0300-EP-0007
+        // Same question after configure reporting done
+        // Same question after bind done
+
+        // Try set color temp (moveToColorTemp)
+        // Auto report ? or read back ? => 0300-EP-0007
+        // Same question after configure reporting done
+    }
 
     logMessage("", "<<< AbeilleColorInspector exiting.");
 ?>
